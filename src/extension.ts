4caff7ccb1d5099c81adda1d5c501ee3f6c6ade3
Gustavo Gutiérrez <ggutierrez@users.noreply.github.com>
2024-07-12 18:00:07 -0500

    retainContext set for memview and registers view.

--- src/extension.ts
+++ src/extension.ts
@@ -18,37 +18,18 @@ export async function activate(context: ExtensionContext) {
   logger().info("Activating extension");
 
   context.subscriptions.push(
-    commands.registerCommand("rv-simulator.sendInstruction", () => {
-      HelloWorldPanel.render(context.extensionUri);
-      // Ask user for input
-      const inst = window.showInputBox({
-        placeHolder: "add x11, x15, x27",
-        value: "add x11, x15, x27",
-        prompt: "Enter a valid RISCV instruction"
-      });
-      // Send input instruction to webview
-      inst.then(function (result) {
-        HelloWorldPanel.getPanel(context.extensionUri)?.postMessage({
-          operation: "runInstruction",
-          operationArgs: {
-            instruction: result
-          }
-        });
-      });
-    })
-  );
-
-  context.subscriptions.push(
     window.registerWebviewViewProvider(
       "rv-simulator.registers",
-      new LeftPanelWebview(context.extensionUri, {})
+      new LeftPanelWebview(context.extensionUri, {}),
+      { webviewOptions: { retainContextWhenHidden: true } }
     )
   );
 
   context.subscriptions.push(
     window.registerWebviewViewProvider(
       "rv-simulator.progmem",
-      ProgMemPanelView.render(context.extensionUri, {})
+      ProgMemPanelView.render(context.extensionUri, {}),
+      { webviewOptions: { retainContextWhenHidden: true } }
     )
   );
 
@@ -72,13 +53,10 @@ export async function activate(context: ExtensionContext) {
       commands.executeCommand("rv-simulator.selectInstructionInMemory");
       const editor = window.activeTextEditor;
       simulateProgram(editor, context.extensionUri);
-      // const progmem = ProgMemPanelView.currentview?.getWebView();
-      // progmem?.postMessage({ operation: "clearMem" });
     })
   );
 
   workspace.onDidSaveTextDocument((document) => {
-    // console.log("Text change");
     const editor = window.activeTextEditor;
     buildAndUploadProgram(editor);
   });
