2db1bd4423796c8f107847a88b73d0a0c3c8493c
Gustavo Gutiérrez <ggutierrez@users.noreply.github.com>
2024-07-12 11:59:51 -0500

    Code cleanup

--- src/webview/main.ts
+++ src/webview/main.ts
@@ -33,8 +33,6 @@ let cpuData = {
 
 function main() {
   log("info", { message: "Initializing simulator events" });
-  // const executeButton = document.getElementById("execute-button") as Button;
-  // executeButton?.addEventListener("click", handleExecute);
   window.addEventListener("message", messageDispatch);
   log("info", { message: "Initialization finished" });
 }
@@ -46,10 +44,6 @@ function messageDispatch(event: MessageEvent) {
       log("executeProgram event ");
       const program = message.program as ParseResult;
       executeProgram(program);
-      // const instTextField = document.getElementById("instText") as TextField;
-      // const executeButton = document.getElementById("execute-button") as Button;
-      // instTextField.value = message.operationArgs.instruction;
-      // executeButton.click();
       break;
     default:
       log("info", { message: "Message not recognized by webview" });
@@ -57,14 +51,39 @@ function messageDispatch(event: MessageEvent) {
   }
 }
 
+/**
+ * Simulator initialization.
+ *
+ * The simulator will execute the instructions of a program once at the time. To
+ * do that it follows a very simple approach:
+ *
+ * - The data memory is initialized along with the registers values. This is
+ *   done before starting any simulation.
+ * - The intermediate representation of the program (IR) is loaded referenced
+ *   from the window object. This representation is an array of objects
+ *   representing every instruction in the source program.
+ * - While there are still instructions to execute:
+ *    - The next instruction IR is stored in window.cpuData.instruction
+ *    - A click event is sent to the (invisible) button in
+ *      window.cpuData.buttonExecute.
+ *    - Every element of the CPU (component, connection, signal) that is
+ *      subscribed to that event will be executed changing its state
+ *      accordingly.
+ *
+ * The statefull components of the cpu will retrieve and persist its data to
+ * their respective objects:
+ *  - The registers unit data is stored in cpuData.ruData
+ *  - The data memory unit data is stored in cpuData.dmData
+ *
+ * The complete output from the parser is stored at: cpuData.parser
+ */
 function executeProgram(program: ParseResult) {
   if (!program.sucess) {
-    log("info", { message: "Nothing to execute, parsing failure." });
+    log("error", { message: "Nothing to execute, parsing failure." });
     return;
   }
   setupSimulatorData(program);
   fetchSVGElements();
-  // // initSVGElements();
   const start = document.getElementById("start-execution") as Button;
   start.addEventListener("click", (e) => {
     start.disabled = true;
@@ -81,7 +100,11 @@ function setupSimulatorData(program: ParseResult) {
   cpuData.instruction = program.ir[0];
   log("info", { message: "simulator data setup finished " });
 }
-
+/**
+ * Fetches all the elements present in the SVG image with the tag "data-cpuName"
+ * defined. Every element is stored in the cpuElements object inside the window
+ * object under the value of the attribute..
+ */
 function fetchSVGElements() {
   log("info", { message: "simulator SVG elements fetch" });
   const elements = document.querySelectorAll(
@@ -97,7 +120,7 @@ function fetchSVGElements() {
     const name = e;
     const elem = (cpuData.cpuElements as any)[name];
     if (name in Handlers) {
-      log("error", { message: "Handler found for " + name });
+      // log("error", { message: "Handler found for " + name });
       (Handlers as any)[name](elem, cpuData);
     }
   });
@@ -108,91 +131,5 @@ function fetchSVGElements() {
     // program: { message: cpuData.parser },
     //instruction: { message: cpuData.instruction },
     //   index: cpuData.instIndex
-  });
-}
-
-function initSVGElements() {
-  log("simulator SVG elements init");
-  const elements = ["CLK"];
-  //const nodes = document.querySelectorAll("#svg-simulator");
-  // const htmlNodes = document.querySelectorAll(
-  //   "#svg-simulator g g [data-cpuname='CLK']"
-  // );
-
-  // elements.forEach((e) => {
-  log("Initializing ${e}");
-  // log(`elements under cpuname=${e}`);
-  // const nodes = document.querySelectorAll(`[data-cpuname="${e}"]`);
-  // nodes.forEach(n => {
-  //   log("hola");
-  // });
-  // Handlers.e()
-  // });
-  // let initialized = 0;
-  // for (const name in window.cpuElements) {
-  //   if (name in Handlers) {
-  //     log("handler initialization ", name);
-  //     initialized++;
-  //     window.cpuElements.state[name] = { enabled: false };
-  //     //     // Call handler for initialization
-  //     const element = window.cpuElements[name];
-  //     const handler = window.cpuHandlers[name] as Function;
-  //     handler(element);
-  //   } else {
-  //     log("Element with no handler", name);
-  //   }
-  // }
-  // log("simulator SVG elements init finished -- initialized ", {initialized: initialized});
-}
-
-function handleExecute() {
-  const instTextField = document.getElementById("instText") as TextField;
-  const instruction = instTextField.value;
-  // debug("Handling instruction " + instruction);
-
-  // parse the instruction here
-
-  vscode.postMessage({
-    command: "hello",
-    text: "Instruction to execute: " + instruction
-  });
-}
-
-// Callback function that is executed when the howdy button is clicked
-function handleHowdyClick() {
-  // Some quick background:
-  //
-  // Webviews are sandboxed environments where abritrary HTML, CSS, and
-  // JavaScript can be executed and rendered (i.e. it's basically an iframe).
-  //
-  // Because of this sandboxed nature, VS Code uses a mechanism of message
-  // passing to get data from the extension context (i.e. src/panels/HelloWorldPanel.ts)
-  // to the webview context (this file), all while maintaining security.
-  //
-  // vscode.postMessage() is the API that can be used to pass data from
-  // the webview context back to the extension context––you can think of
-  // this like sending data from the frontend to the backend of the extension.
-  //
-  // Note: If you instead want to send data from the extension context to the
-  // webview context (i.e. backend to frontend), you can find documentation for
-  // that here:
-  //
-  // https://code.visualstudio.com/api/extension-guides/webview#passing-messages-from-an-extension-to-a-webview
-  //
-  // The main thing to note is that postMessage() takes an object as a parameter.
-  // This means arbitrary data (key-value pairs) can be added to the object
-  // and then accessed when the message is recieved in the extension context.
-  //
-  // For example, the below object could also look like this:
-  //
-  // {
-  //  command: "hello",
-  //  text: "Hey there partner! 🤠",
-  //  random: ["arbitrary", "data"],
-  // }
-  //
-  vscode.postMessage({
-    command: "hello",
-    text: "Hey there partner! 🤠"
   });
 }
