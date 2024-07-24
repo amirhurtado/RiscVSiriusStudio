177b1d3cef4ab696549ffc05bae0ec84ca2252ff
Gustavo Gutiérrez <ggutierrez@users.noreply.github.com>
2024-07-24 09:24:59 -0500

    Added message dispatcher and quote standarization.

--- src/registersview/registersview.ts
+++ src/registersview/registersview.ts
@@ -2,14 +2,14 @@ import {
   provideVSCodeDesignSystem,
   allComponents,
   Checkbox
-} from "@vscode/webview-ui-toolkit";
+} from '@vscode/webview-ui-toolkit';
 
 import {
   CellComponent,
   GroupComponent,
   RowComponent,
   TabulatorFull as Tabulator
-} from "tabulator-tables";
+} from 'tabulator-tables';
 
 import {
   binaryToUInt,
@@ -23,12 +23,12 @@ import {
   validBinary,
   validAscii,
   toBinary
-} from "../utilities/conversions";
+} from '../utilities/conversions';
 
 provideVSCodeDesignSystem().register(allComponents);
 
 const vscode = acquireVsCodeApi();
-window.addEventListener("load", main);
+window.addEventListener('load', main);
 
 /**
  * Log functionality. The logger that is actually used is in the extension. This
@@ -39,7 +39,7 @@ window.addEventListener("load", main);
  * @param object the object to be logged/
  */
 function log(kind: string, object: any = {}) {
-  sendMessageToExtension({ command: "log-" + kind, obj: { object } });
+  sendMessageToExtension({ command: 'log-' + kind, obj: { object } });
 }
 
 /**
@@ -51,11 +51,12 @@ function log(kind: string, object: any = {}) {
  * - 16: as an hexadecimal number
  * - ascii: as a sequence of 8 bits ascii characters (left to right)
  */
-const possibleViews = [2, "signed", "unsigned", 16, "ascii"];
+const possibleViews = [2, 'signed', 'unsigned', 16, 'ascii'];
 type RegisterView = typeof possibleViews[number];
 
 type RegisterValue = {
   name: string;
+  rawName: string;
   value: string;
   rawValue: string;
   watched: boolean;
@@ -67,85 +68,105 @@ type RegisterValue = {
 function main() {
   let table = tableSetup();
   sortCriteria(table);
+  // Message dispatcher
+  window.addEventListener('message', (event) => {
+    dispatch(event, table);
+  });
+}
+
+function dispatch(event: MessageEvent, table: Tabulator) {
+  const data = event.data;
+  switch (data.operation) {
+    case 'selectRegister':
+      selectRegister(data.register, table);
+      break;
+    case 'clearSelection':
+      table.deselectRow();
+      break;
+  }
+}
+
+function selectRegister(register: string, table: Tabulator) {
+  table.selectRow(register);
 }
 
 function tableSetup(): Tabulator {
   const registers = [
-    "x0 zero",
-    "x1 ra",
-    "x2 sp",
-    "x3 gp",
-    "x4 tp",
-    "x5 t0",
-    "x6 t1",
-    "x7 t2",
-    "x8 s0",
-    "x9 s1",
-    "x10 a0",
-    "x11 a1",
-    "x12 a2",
-    "x13 a3",
-    "x14 a4",
-    "x15 a5",
-    "x16 a6",
-    "x17 a7",
-    "x18 s2",
-    "x19 s3",
-    "x20 s4",
-    "x21 s5",
-    "x22 s6",
-    "x23 s7",
-    "x24 s8",
-    "x25 s9",
-    "x26 s10",
-    "x27 s11",
-    "x28 t3",
-    "x29 t4",
-    "x30 t5",
-    "x31 t6"
+    'x0 zero',
+    'x1 ra',
+    'x2 sp',
+    'x3 gp',
+    'x4 tp',
+    'x5 t0',
+    'x6 t1',
+    'x7 t2',
+    'x8 s0',
+    'x9 s1',
+    'x10 a0',
+    'x11 a1',
+    'x12 a2',
+    'x13 a3',
+    'x14 a4',
+    'x15 a5',
+    'x16 a6',
+    'x17 a7',
+    'x18 s2',
+    'x19 s3',
+    'x20 s4',
+    'x21 s5',
+    'x22 s6',
+    'x23 s7',
+    'x24 s8',
+    'x25 s9',
+    'x26 s10',
+    'x27 s11',
+    'x28 t3',
+    'x29 t4',
+    'x30 t5',
+    'x31 t6'
   ];
   let tableData = [] as Array<RegisterValue>;
-  let table = new Tabulator("#registers-table", {
-    maxHeight: "80vh",
+  let table = new Tabulator('#registers-table', {
+    maxHeight: '80vh',
     data: tableData,
-    layout: "fitColumns",
+    layout: 'fitColumns',
     layoutColumnsOnNewData: true,
-    index: "rawName",
+    index: 'rawName',
     reactiveData: true,
-    groupBy: "watched",
+    groupBy: 'watched',
     groupValues: [[true, false]],
     groupHeader: hederGrouping,
     groupUpdateOnCellEdit: true,
     movableRows: true,
-    validationMode: "blocking",
+    validationMode: 'blocking',
     columns: [
       {
-        title: "Name",
-        field: "name",
+        title: 'Name',
+        field: 'name',
         visible: true,
         headerSort: false,
-        cssClass: "register-name",
+        cssClass: 'register-name',
         frozen: true,
         width: 90,
         formatter: registerNamesFormatter
       },
       {
-        title: "Value",
-        field: "value",
+        title: 'Value',
+        field: 'value',
         visible: true,
         headerSort: false,
-        cssClass: "register-value",
+        cssClass: 'register-value',
         formatter: valueFormatter,
         editor: valueEditor,
         editable: editableValue
       },
       {
-        title: "",
-        field: "viewType",
+        title: '',
+        field: 'viewType',
         visible: true,
         width: 60,
         headerSort: false,
-        editor: "list",
+        editor: 'list',
         cellEdited: viewTypeEdited,
         editorParams: {
           values: possibleViews,
@@ -154,17 +175,19 @@ function tableSetup(): Tabulator {
         },
         formatter: viewTypeFormatter
       },
-      { title: "Watched", field: "watched", visible: false },
-      { title: "Modified", field: "modified", visible: false },
-      { title: "id", field: "id", visible: false }
+      { title: 'Watched', field: 'watched', visible: false },
+      { title: 'Modified', field: 'modified', visible: false },
+      { title: 'id', field: 'id', visible: false },
+      { title: 'rawName', field: 'rawName', visible: true }
     ]
   });
 
   registers.forEach((e, idx) => {
-    const [xname, abi] = e.split(" ");
-    const zeros32 = "0";
+    const [xname, abi] = e.split(' ');
+    const zeros32 = '0';
     tableData.push({
       name: `${xname} ${abi}`,
+      rawName: `${xname}`,
       value: zeros32,
       rawValue: zeros32,
       viewType: 2,
@@ -174,8 +197,9 @@ function tableSetup(): Tabulator {
     });
   });
 
-  table.on("rowDblClick", toggleWatched);
-  table.on("cellEdited", modifiedCell);
+  table.on('rowDblClick', toggleWatched);
+  table.on('cellEdited', modifiedCell);
+  table.on('cellEdited', notifyExtension);
   return table;
 }
 
@@ -185,7 +209,7 @@ function tableSetup(): Tabulator {
  */
 function editableValue(cell: CellComponent) {
   const { name } = cell.getRow().getData();
-  return name !== "x0 zero";
+  return name !== 'x0 zero';
 }
 
 /**
@@ -206,13 +230,13 @@ function viewTypeEdited(cell: CellComponent) {
  */
 function formatValueAsType(value: string, type: RegisterView): string {
   switch (type) {
-    case "unsigned":
+    case 'unsigned':
       return binaryToUInt(value);
-    case "signed":
+    case 'signed':
       return binaryToInt(value);
     case 16:
       return binaryToHex(value);
-    case "ascii":
+    case 'ascii':
       return binaryToAscii(value);
   }
   // type must be binary
@@ -239,16 +263,16 @@ function valueEditor(
 ) {
   const { name, value, viewType } = cell.getRow().getData();
 
-  log("info", {
-    msg: "valueEditor called",
+  log('info', {
+    msg: 'valueEditor called',
     rawValue: value,
     currentVType: viewType,
     reg: name
   });
   const viewValue = formatValueAsType(value, viewType);
 
-  let editor = document.createElement("input");
-  editor.className = "register-editor";
+  let editor = document.createElement('input');
+  editor.className = 'register-editor';
   editor.value = viewValue;
   editor.select();
 
@@ -259,8 +283,8 @@ function valueEditor(
   function successFunc() {
     const newValue = editor.value;
     const valid = isValidAs(newValue, viewType);
-    log("info", {
-      msg: "called success function",
+    log('info', {
+      msg: 'called success function',
       check: valid,
       newValue: newValue,
       type: viewType
@@ -270,14 +294,14 @@ function valueEditor(
       success(bin);
     } else {
       editor.focus();
-      editor.className = "register-editor-error";
+      editor.className = 'register-editor-error';
     }
   }
 
-  editor.addEventListener("change", successFunc);
-  editor.addEventListener("blur", successFunc);
-  editor.addEventListener("keydown", (evt) => {
-    if (evt.key === "Escape") {
+  editor.addEventListener('change', successFunc);
+  editor.addEventListener('blur', successFunc);
+  editor.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
       cancel();
     }
   });
@@ -294,16 +318,16 @@ function isValidAs(value: string, valType: RegisterView) {
   switch (valType) {
     case 2:
       return validBinary(value);
-    case "unsigned":
+    case 'unsigned':
       return validUInt32(value);
-    case "signed":
+    case 'signed':
       return validInt32(value);
     case 16:
       return validHex(value);
-    case "ascii":
+    case 'ascii':
       return validAscii(value);
   }
-  log("info", { msg: "none of the test matched", type: valType });
+  log('info', { msg: 'none of the test matched', type: valType });
   return false;
 }
 
@@ -324,10 +348,10 @@ function valueFormatter(
   switch (viewType) {
     case 2:
       return binaryRepresentation(value);
-    case "signed": {
+    case 'signed': {
       return binaryToInt(value);
     }
-    case "unsigned": {
+    case 'unsigned': {
       const rvalue = binaryToUInt(value);
       return rvalue;
     }
@@ -335,7 +359,7 @@ function valueFormatter(
       const rvalue = binaryToHex(value);
       return rvalue;
     }
-    case "ascii": {
+    case 'ascii': {
       return binaryToAscii(value);
     }
   }
@@ -357,8 +381,8 @@ function registerNamesFormatter(
   onRendered: any
 ) {
   const { name } = cell.getData();
-  const [xname, abiname] = name.split(" ");
-  return xname + " (" + abiname + ")";
+  const [xname, abiname] = name.split(' ');
+  return xname + ' (' + abiname + ')';
 }
 
 /**
@@ -374,19 +398,19 @@ function viewTypeFormatter(
   onRendered: any
 ) {
   const { viewType } = cell.getData();
-  let tag: string = "";
+  let tag: string = '';
   switch (viewType) {
     case 2:
-      tag = "bin";
+      tag = 'bin';
       break;
-    case "unsigned":
-      tag = "10";
+    case 'unsigned':
+      tag = '10';
       break;
-    case "signed":
-      tag = "±10";
+    case 'signed':
+      tag = '±10';
       break;
     case 16:
-      tag = "hex";
+      tag = 'hex';
       break;
     default:
       tag = viewType;
@@ -406,6 +430,17 @@ function modifiedCell(cell: CellComponent) {
   cell.getRow().update({ modified: Date.now() });
 }
 
+function notifyExtension(cell: CellComponent) {
+  const { rawName, value } = cell.getData();
+  log('info', { msg: 'notify extension', name: rawName, val: value });
+  sendMessageToExtension({
+    command: 'event',
+    from: 'registerView',
+    message: 'registerUpdate',
+    name: rawName,
+    value: value
+  });
+}
 /**
  * Toggles the value of the watched field in a row
  * @param event
@@ -413,8 +448,9 @@ function modifiedCell(cell: CellComponent) {
  */
 function toggleWatched(event: UIEvent, row: RowComponent) {
   const { rawName: rn, watched: w } = row.getData();
+  log('info', { message: 'updXXXXXate error', rawName: rn, watched: w });
   row.update({ rawName: rn, watched: !w }).catch((error) => {
-    log("info", { message: "update error", rawName: rn, watched: w });
+    log('info', { message: 'update error', rawName: rn, watched: w });
   });
 }
 
@@ -429,11 +465,11 @@ function hederGrouping(
   data: any,
   group: GroupComponent
 ) {
-  let watchStr = "Watched";
+  let watchStr = 'Watched';
   if (!value) {
-    watchStr = "Unwatched";
+    watchStr = 'Unwatched';
   }
-  return watchStr + "  (" + count + " registers)";
+  return watchStr + '  (' + count + ' registers)';
 }
 
 /**
@@ -443,12 +479,12 @@ function hederGrouping(
  */
 function sortTable(table: Tabulator) {
   const lastModifiedCB = document.getElementById(
-    "sort-last-modified"
+    'sort-last-modified'
   ) as Checkbox;
   if (lastModifiedCB.checked) {
-    table.setSort("modified", "desc");
+    table.setSort('modified', 'desc');
   } else {
-    table.setSort("id", "asc");
+    table.setSort('id', 'asc');
   }
 }
 
@@ -457,14 +493,14 @@ function sortTable(table: Tabulator) {
  * @param table table to sort
  */
 function sortCriteria(table: Tabulator) {
-  table.on("cellEdited", () => {
+  table.on('cellEdited', () => {
     sortTable(table);
   });
 
   const lastModifiedCB = document.getElementById(
-    "sort-last-modified"
+    'sort-last-modified'
   ) as Checkbox;
-  lastModifiedCB.addEventListener("change", () => {
+  lastModifiedCB.addEventListener('change', () => {
     sortTable(table);
   });
 }
