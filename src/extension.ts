d4c621b72f9792e3c59fda13447d190dc2dcbe5c
Gustavo Gutiérrez <ggutierrez@users.noreply.github.com>
2024-08-12 13:47:16 -0500

    Code cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -65,71 +65,10 @@ export function activate(context: ExtensionContext) {
     })
   );
 
-  context.subscriptions.push(
-    commands.registerCommand('rv-simulator.enableProgMemSync', () => {
-      console.log('executing progmemsync command');
-      /**
-       * To handle synchronization we need to track changes of the cursor in the
-       * text editor to reflect changes on the program code. Due to this bug or
-       * functionality: https://github.com/microsoft/vscode/issues/181233 The
-       * event is triggered when the cursor changes but not necessarily on the
-       * file we need. For that reason we use the rvContext to stroe filename
-       * and caret line position and decide whnever we are interested in the
-       * change.
-       */
-      window.onDidChangeTextEditorSelection(
-        (event: TextEditorSelectionChangeEvent) => {
-          const editor = event.textEditor;
-          const fileName = editor.document.fileName;
-          if (RVExtensionContext.isValidFile(editor.document)) {
-            if (rvContext.getCurrentFile() !== fileName) {
-              rvContext.setCurrentFile(fileName);
-              buildAndUploadProgram(editor);
-            }
-            const currentLine = editor.selection.active.line;
-            if (rvContext.lineChanged(currentLine)) {
-              highlightInstructionInMemory(editor);
-
-              // other way
-              if (editor) {
-                const ir = buildCurrent(editor);
-                console.log('IMPORTANT!!! ', ir);
-                const inst = getIRForInstructionAt(currentLine, ir);
-              }
-            }
-          }
-        }
-      );
-    })
-  );
   /**
    * Reflect changes of vscode files to the extension context.
    *
    */
-
-  /**
-   * When a document is opened change the context current file.
-   */
-  // workspace.onDidOpenTextDocument((document) => {
-  //   if (RVExtensionContext.isValidFile(document)) {
-  //     const name = document.fileName;
-  //     rvContext.setCurrentFile(name);
-  //   }
-  // });
-
-  // workspace.onDidCloseTextDocument((document) => {
-  //   if (RVExtensionContext.isValidFile(document)) {
-  //     rvContext.setAndBuildCurrentFile(undefined);
-  //   }
-  // });
-
-  // workspace.onDidSaveTextDocument((document) => {
-  //   console.log('Save editor event');
-  //   if (RVExtensionContext.isValidFile(document)) {
-  //     const editor = window.activeTextEditor;
-  //     buildAndUploadProgram(editor);
-  //   }
-  // });
 
   workspace.onDidChangeConfiguration((e: ConfigurationChangeEvent) => {
     console.log('Configuration change occurred.');
@@ -152,9 +91,6 @@ export function activate(context: ExtensionContext) {
       settings: { sort: sort }
     });
   });
-
-  // enable synchronization
-  // commands.executeCommand('rv-simulator.enableProgMemSync');
 
   /**
    * This will build the program every time the file is saved.
@@ -270,51 +206,6 @@ function simulateProgram(
 function buildProgram(source: string, sourceName: string): ParserResult {
   const result = compile(source, sourceName);
   return result;
-}
-
-function buildCurrent(editor: TextEditor): any {
-  const source = editor.document.getText();
-  const fileName = editor.document.fileName;
-  const result = compile(source, fileName);
-  if (!result.sucess) {
-    window.showErrorMessage('Build failure!');
-  } else {
-    return result.ir;
-  }
-}
-
-function buildAndUploadProgram(editor: TextEditor | undefined) {
-  if (editor) {
-    const document = editor.document;
-    const inputsrc = document.getText();
-    const result = buildProgram(inputsrc, document.fileName);
-    if (!result.sucess) {
-      window.showErrorMessage('Build failure!');
-    } else {
-      const progmem = ProgMemPanelView.currentview?.getWebView();
-      progmem?.postMessage({ operation: 'updateProgram', program: result.ir });
-      // console.log(result.ir);
-    }
-  }
-}
-
-function highlightInstructionInMemory(editor: TextEditor | undefined) {
-  console.log('highlight instruction called');
-  if (editor) {
-    if (editor.document.isDirty) {
-      window.showInformationMessage(
-        'Source is out of sync. Save to synchronize with memory view'
-      );
-      sendMessageToProgMemView({ operation: 'clearProgMemSelections' });
-    } else {
-      const position = editor.selection.active;
-      const line = position.line;
-      sendMessageToProgMemView({
-        operation: 'selectInstruction',
-        sourceLine: line
-      });
-    }
-  }
 }
 
 function sendMessageToProgMemView(msg: any) {
