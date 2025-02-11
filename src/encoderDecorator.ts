d10f59a8d0cd8ce269d10fab42169bcc5eb4c80f
Gustavo Gutiérrez <ggutierrez@users.noreply.github.com>
2025-02-11 15:42:14 -0500

    using async for better performance when decorating

--- src/encoderDecorator.ts
+++ src/encoderDecorator.ts
@@ -1,13 +1,32 @@
-import { TextDocument, TextEditor, window } from "vscode";
+import { MarkdownString, TextDocument, TextEditor, TextEditorDecorationType, window } from "vscode";
 import { RVDocument } from "./rvDocument";
-import { kMaxLength } from "buffer";
+import internal from "stream";
+import { InternalRepresentation } from "./utilities/riscvc";
+
+const encoderDecoration: TextEditorDecorationType = window.createTextEditorDecorationType({
+  after: {
+    margin: '0 0 0 3em',
+    textDecoration: 'none',
+    // color: '#999999'
+  }
+});
+
+async function detailsMessage(ir: any | undefined): Promise<MarkdownString | undefined> {
+  const markdown = new MarkdownString();
+  markdown.isTrusted = true;
+  markdown.supportHtml = true;
+  markdown.supportThemeIcons = true;
+  if (!ir) {
+    markdown.appendMarkdown(`<h4>Undefined</h4>\n`);
+  } else {
+    markdown.appendMarkdown(`<h4>Encoding for ${ir["asm"]}</h4>\n`);
+    markdown.appendMarkdown(`<h5>${ir["asm"]}</h5>\n`);
+  }
+  return markdown;
+}
 
 export class EncoderDecorator {
-  private decorationType = window.createTextEditorDecorationType({
-    after: {
-      color: '#999999'
-    }
-  });
+
 
   private static maxLength(document: TextDocument): number {
     let max = 0;
@@ -32,36 +51,41 @@ export class EncoderDecorator {
     return decorationText;
   }
 
-  public decorate(editor: TextEditor, rvDoc: RVDocument) {
+  public async decorate(editor: TextEditor, rvDoc: RVDocument) {
     console.log("####################");
     console.log("Decorating ", rvDoc.document.uri.toString());
     console.log("####################");
-    const decorations = [];
 
     const ml = EncoderDecorator.maxLength(editor.document);
 
-    for (let i = 0; i < editor.document.lineCount; i++) {
-      const line = editor.document.lineAt(i);
-      const ir = rvDoc.getIRForLine(i);
-      decorations.push({
-        range: line.range,
-        hoverMessage: line.text,
-        renderOptions: {
-          isWholeLine: true,
-          after: {
-            contentText: this.getDecorationForLine(line.text, i, ir, rvDoc),
-            margin: `0 0 0 ${(ml - line.text.length + 4) * 10}px`,
-            fontWeight: 'bold',
-            textAlign: 'left',
-            opacity: 0.5
+    async function updateDecorations() {
+      const decorations = [];
+      for (let i = 0; i < editor.document.lineCount; i++) {
+        const line = editor.document.lineAt(i);
+        const ir = rvDoc.getIRForLine(i);
+        const irText = ir ? ir.encoding.binEncoding : '';
+        decorations.push({
+          range: line.range,
+          hoverMessage: await detailsMessage(ir),
+          renderOptions: {
+            // isWholeLine: true,
+            after: {
+              contentText: irText,
+              margin: `0 0 0 ${(ml - line.text.length + 4) * 10}px`,
+              // fontWeight: 'bold',
+              textAlign: 'right',
+              opacity: 0.5
+            }
           }
-        }
-      });
+        });
+      }
+      editor.setDecorations(encoderDecoration, decorations);
     }
-    editor.setDecorations(this.decorationType, decorations);
+
+    await updateDecorations();
   }
 
   public clearDecorations(editor: TextEditor) {
-    editor.setDecorations(this.decorationType, []);
+    editor.setDecorations(encoderDecoration, []);
   }
 }
