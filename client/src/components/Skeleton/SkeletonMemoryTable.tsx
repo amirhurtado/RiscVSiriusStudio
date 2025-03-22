dcb70a27b645cdde13f7b6347e55f0cdd2e52d51
Amir <a.hurtado@utp.edu.co>
2025-03-22 17:21:54 -0500

    Correction in skeleton structure of memory table

--- client/src/components/Skeleton/SkeletonMemoryTable.tsx
+++ client/src/components/Skeleton/SkeletonMemoryTable.tsx
@@ -3,23 +3,25 @@ import { Skeleton } from "@/components/ui/skeleton";
 const SkeletonMemoryTable = () => {
 
   const repeatedRowsCount = 15;
-  const repeatedColCount = 4;
+  const repeatedColCount = 5;
 
   return (
     <div className="min-w-[34.8rem] max-h-[calc(100dvh-2.3rem)] overflow-hidden z-100 absolute top-0 left-0">
      
-        <div className="flex flex-col gap-[.7rem]">
+        <div className="flex flex-col gap-[.6rem]">
           {Array.from({ length: repeatedRowsCount }).map((_, rowIndex) => (
-            <div key={`row-${rowIndex}`} className="flex gap-[.4rem]">
+            <div key={`row-${rowIndex}`} className="flex gap-[.5rem]">
               <Skeleton
-                  className="h-8 w-[4rem]"
+                  className="h-8 w-[3.6rem]"
                 />
+              <div className="flex gap-[.8rem]">
               {Array.from({ length: repeatedColCount }).map((_, colIndex) => (
                 <Skeleton
                   key={`row-${rowIndex}-col-${colIndex}`}
-                  className="h-8 w-[5rem]"
+                  className="h-8 w-[4.3rem]"
                 />
               ))}
+              </div>
               <Skeleton
                 className="h-8 w-[7rem]"
               />
