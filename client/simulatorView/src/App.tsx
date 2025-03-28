955f20aee92da8e2ccd073994aa6ea376767f7fe
Amir Hurtado <a.hurtado@utp.edu.co>
2025-03-28 14:50:48 -0500

    Creating a canvas with a basic example V3

--- client/simulatorView/src/App.tsx
+++ client/simulatorView/src/App.tsx
@@ -1,11 +1,11 @@
 //import { ThemeProvider } from "@/components/ui/theme/theme-provider";
-import MyCanva from "./components/Canvas";
+import MyCanvas from "./components/Canvas";
 
 const App = () => {
   return (
     //<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="relative flex w-full h-full overflow-hidden ">
-          <MyCanva /> {/* here we import the canvas component */}
+          <MyCanvas /> {/* here we import the canvas component */}
       </div>
     //</ThemeProvider>
   );
