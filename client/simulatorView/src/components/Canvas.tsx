dd585bba02e5b7b65201e805f2f11f58ef4c01d5
Amir Hurtado <a.hurtado@utp.edu.co>
2025-03-28 14:50:48 -0500

    Creating a canvas with a basic example V3

--- client/simulatorView/src/components/Canvas.tsx
+++ client/simulatorView/src/components/Canvas.tsx
@@ -18,7 +18,7 @@ const initialNodes = [
 ];
 const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
-export default function App() {
+export default function MyCanvas() {
   const [nodes, _setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
