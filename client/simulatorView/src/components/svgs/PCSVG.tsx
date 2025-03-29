12f82485ed8752201ecb801fbbdb54f3022534dc
Amir <a.hurtado@utp.edu.co>
2025-03-28 23:15:41 -0500

    Creating a long SVG container

--- client/simulatorView/src/components/svgs/PCSVG.tsx
+++ client/simulatorView/src/components/svgs/PCSVG.tsx
@@ -1,10 +1,10 @@
-import ContainerSVG from './ContainerSvg';
 import { Handle, Position } from '@xyflow/react';
+import LargeContainer from './LargeContainer';
 
 export default function PCSVG() {
   return (
     <div style={{ position: 'relative' }}>
-        <ContainerSVG />
+        <LargeContainer />
         <Handle  type="source"
         position={Position.Right}
         style={{ transform: 'translateX(-50%)' }}/>
