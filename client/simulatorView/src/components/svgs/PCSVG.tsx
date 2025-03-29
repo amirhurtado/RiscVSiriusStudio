c74646b3d51188545d36611e576dfd57d3eaec6f
Amir <a.hurtado@utp.edu.co>
2025-03-29 09:12:21 -0500

    PC customization and instruction memory

--- client/simulatorView/src/components/svgs/PCSVG.tsx
+++ client/simulatorView/src/components/svgs/PCSVG.tsx
@@ -1,10 +1,18 @@
 import { Handle, Position } from '@xyflow/react';
-import LargeContainer from './LargeContainer';
+import LargeContainerSVG from './containers/LargeContainerSVG';
+import {Triangle} from 'lucide-react';
 
 export default function PCSVG() {
   return (
-    <div style={{ position: 'relative' }}>
-        <LargeContainer />
+    <div className=''>
+
+       <div className='relative w-full h-full'>
+       <h2 className="text-2xl text-[#4A4A4A] absolute top-[50%] left-[45%] transform -translate-x-[45%] -translate-y-[50%] z-2">PC</h2>
+        <LargeContainerSVG  />
+        <Triangle size={20} className='absolute left-[45%]  transform -translate-x-[45%] text-[#404040] bottom-[.4rem] z-2 ' />
+       </div>
+        
+
         <Handle  type="source"
         position={Position.Right}
         style={{ transform: 'translateX(-50%)' }}/>
