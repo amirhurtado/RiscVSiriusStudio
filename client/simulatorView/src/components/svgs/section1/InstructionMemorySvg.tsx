5629705e821f4d2ae2e20eab8b97c31e993419cf
Amir <a.hurtado@utp.edu.co>
2025-03-29 13:01:29 -0500

    Creation of first connection from instruction memory to registersUnit

--- client/simulatorView/src/components/svgs/section1/InstructionMemorySvg.tsx
+++ client/simulatorView/src/components/svgs/section1/InstructionMemorySvg.tsx
@@ -5,12 +5,15 @@ export default function InstructionMemorySvg() {
   return (
     <div style={{ position: 'relative' }}>
       <div className='relative w-full h-full'>
-       <h2 className="text-2xl text-[#4A4A4A] absolute top-[15%] left-[45%] transform -translate-x-[45%] -translate-y-[15%] z-2">Instruction Memory</h2>
+       <h2 className="text-xl text-[#555555] absolute top-[15%] left-[45%] transform -translate-x-[45%] -translate-y-[15%] z-2">Instruction Memory</h2>
         <ContainerSVG />
        </div>
       <Handle  type="target"
         position={Position.Left}
         style={{ transform: 'translateX(-50%)' }} />
+      <Handle  type="source"
+        position={Position.Right}
+        style={{ top: '10rem', background: '#555', width: 10, height: 10 }} />
       </div>
   );
 }
