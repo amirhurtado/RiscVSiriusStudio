cedcb5ed92387528153179eb5d7a0509a722c2b9
Amir <a.hurtado@utp.edu.co>
2025-03-29 23:17:29 -0500

    the space of elements is changed

--- client/simulatorView/src/components/sections/data/nodes/section2.ts
+++ client/simulatorView/src/components/sections/data/nodes/section2.ts
@@ -3,145 +3,120 @@
 */
 
 import {
-  Node,
-} from '@xyflow/react';
+    Node,
+  } from '@xyflow/react';
 
 
 export const section2: Node[] = [
-  {
-    id: 'section-2',
-    type: 'group',
-    data: { label: 'Section 2' },
-    position: { x: 548, y: 0 },
-    draggable: false,
-    style: {
-      width: 548,
-      height: 1200,
-      backgroundColor: '#FFF9C4',
-      border: '1px solid #eed97f',
-      borderRadius: 8,
-    },
-  },
-
-  // CHILD NODES SECTION 2
-  {
-    id: 'controlUnit',
-    type: 'controlUnit',
-    data: { label: 'Control Unit' },
-    position: { x: 280, y: 255 },
-    parentId: 'section-2',
-    extent: 'parent',
-    style: {
-      width: 170,
-      height: 220,
-      backgroundColor: 'transparent',
-      border: 'none',
-      borderRadius: 0,
-      padding: 0,
-      boxShadow: 'none',
-    },
-  },
-  },
-
-
-{
-  id: 'registersUnit',
-    type: 'registerUnit',
-      data: { label: 'Registers Unit' },
-  position: { x: 280, y: 635 },
-  parentId: 'section-2',
-    extent: 'parent',
+    {
+      id: 'section-2',
+      type: 'group',
+      data: { label: 'Section 2' },
+      position: { x: 548, y: 0 },
+      draggable: false,
       style: {
-    width: 240,
-      height: 300,
-        backgroundColor: 'transparent',
-          border: 'none',
-            borderRadius: 0,
-              padding: 0,
-                boxShadow: 'none',
+        width: 548,
+        height: 1200,
+        backgroundColor: '#FFF9C4',
+        border: '1px solid #eed97f',
+        borderRadius: 8,
       },
-},
-  },
+    },
 
-{
-  id: 'immSrc',
-    type: 'immSrc',
+     // CHILD NODES SECTION 2
+    {
+      id: 'controlUnit',
+      type: 'controlUnit',
+      data: { label: 'Control Unit' },
+      position: { x: 280, y: 140 },
+      parentId: 'section-2',
+      extent: 'parent',
+      style: {
+        width: 170,
+        height: 220,
+        backgroundColor: 'transparent',
+        border: 'none',
+        borderRadius: 0,
+        padding: 0,
+        boxShadow: 'none',
+      },
+    },
+
+
+    {
+      id: 'registersUnit',
+      type: 'registerUnit',
+      data: { label: 'Registers Unit' },
+      position: { x: 280, y: 475 },
+      parentId: 'section-2',
+      extent: 'parent',
+      style: {
+        width: 240,
+        height: 300,
+        backgroundColor: 'transparent',
+        border: 'none',
+        borderRadius: 0,
+        padding: 0,
+        boxShadow: 'none',
+      },
+    },
+
+    {
+      id: 'immSrc',
+      type: 'immSrc',
       data: { label: 'IMMSrc' },
-  position: { x: 133, y: 1078 },
-  parentId: 'section-2',
-    extent: 'parent',
+      position: { x: 133, y: 1078},
+      parentId: 'section-2',
+      extent: 'parent',
       style: {
-    width: 90,
-      height: 30,
+        width: 90,
+        height: 30,
         backgroundColor: 'transparent',
-          border: 'none',
-            borderRadius: 0,
-              padding: 0,
-                boxShadow: 'none',
-    },
-},
-
-{
-  id: 'immGenerator',
-    type: 'immGenerator',
-      data: { label: 'Registers Unit' },
-  position: { x: 280, y: 1025 },
-  parentId: 'section-2',
-    extent: 'parent',
-      style: {
-    width: 240,
-      height: 100,
-        backgroundColor: 'transparent',
-          border: 'none',
-            borderRadius: 0,
-              padding: 0,
-                boxShadow: 'none',
+        border: 'none',
+        borderRadius: 0,
+        padding: 0,
+        boxShadow: 'none',
       },
-},
-  },
-
-//PIVOTS S2
-//NODE INSTRUCTIONMEMORY-PIVOT-REGISTERSUNIT
-
-{
-  id: 'pivot3',
-    type: 'pivot3',
-      data: { label: '' },
-  position: { x: 0, y: 670 },
-  parentId: 'section-2',
-    extent: 'parent',
-      style: {
-    width: 1,
-      height: 1,
-        opacity: 0,
-          backgroundColor: 'transparent',
-            border: 'none',
-              borderRadius: 0,
-                padding: 0,
-                  boxShadow: 'none',
     },
-},
 
-
-//jump
-{
-  id: 'pivotJump1',
-    type: 'pivotJump1',
-      data: { label: 'pivotJump1' },
-  position: { x: 120, y: 410 },
-  parentId: 'section-2',
-    extent: 'parent',
+    {
+      id: 'immGenerator',
+      type: 'immGenerator',
+      data: { label: 'Registers Unit' },
+      position: { x: 280, y: 1025 },
+      parentId: 'section-2',
+      extent: 'parent',
       style: {
-    width: 47,
-      height: 47,
+        width: 240,
+        height: 100,
         backgroundColor: 'transparent',
-          border: 'none',
-            borderRadius: 0,
-              padding: 0,
-                boxShadow: 'none',
+        border: 'none',
+        borderRadius: 0,
+        padding: 0,
+        boxShadow: 'none',
+      },
     },
-},
 
+    //PIVOTS S2
+    //NODE INSTRUCTIONMEMORY-PIVOT-REGISTERSUNIT
 
-
-];
+    {
+      id: 'instMemPivotRU',
+      type: 'instMemPivotRU',
+      data: { label: '' },
+      position: { x: 0, y: 670 },
+      parentId: 'section-2',
+      extent: 'parent',
+      style: {
+        width: 1,
+        height: 1,
+        opacity: 0,
+        backgroundColor: 'transparent',
+        border: 'none',
+        borderRadius: 0,
+        padding: 0,
+        boxShadow: 'none',
+      },
+    },
+   
+  ];
