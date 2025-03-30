60c3928a35146161bb11d35bdcc7fc1a6e8ed182
Amir <a.hurtado@utp.edu.co>
2025-03-30 17:57:16 -0500

    Refactor PC_MUXA edges and update pivot positions for improved flow

--- client/simulatorView/src/components/sections/data/edges/PC_MUXA.ts
+++ client/simulatorView/src/components/sections/data/edges/PC_MUXA.ts
@@ -5,7 +5,8 @@ import { Edge, MarkerType  } from '@xyflow/react';
 export const PC_MUXA: Edge[] = [
 
     { id: 'pivot1->pivotJump1',  source: 'pivot1', target: 'pivotJump1', sourceHandle: 'muxA',  type: 'smoothstep', animated:true},
-    { id: 'pivotJump1->pivot2',  source: 'pivotJump1', target: 'pivot2',  type: 'smoothstep', animated:true},
+    { id: 'pivotJump1->pivotJump2',  source: 'pivotJump1', target: 'pivotJump2',  type: 'smoothstep', animated:true},
+    { id: 'pivotJump2->pivot2',  source: 'pivotJump2', target: 'pivot2',  type: 'smoothstep', animated:true},
     { id: 'pivot2->muxA',  source: 'pivot2', target: 'muxA', targetHandle: 'pc',  type: 'smoothstep', animated:true, markerEnd: {
         type: MarkerType.ArrowClosed,
         width: 18,
