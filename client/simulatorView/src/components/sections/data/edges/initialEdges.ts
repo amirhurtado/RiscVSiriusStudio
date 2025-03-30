635d0ac1c6beeaf5e1517bb29a1c6ff1ae0973ed
Amir <a.hurtado@utp.edu.co>
2025-03-29 21:59:52 -0500

    Connection imm generator with mux2_1b

--- client/simulatorView/src/components/sections/data/edges/initialEdges.ts
+++ client/simulatorView/src/components/sections/data/edges/initialEdges.ts
@@ -3,39 +3,22 @@
 * Each edge has an id, source node, target node, and type.
 */
 
-import { Edge, MarkerType  } from '@xyflow/react';
+import { Edge  } from '@xyflow/react';
 import { IM_RU } from './IM_RU';
 import { IM_CU } from './IM_CU';
 import { PC_ADDR4 } from './PC_ADDR4';
 import { IM_IG } from './IM_IG';
 import { RU_MUX2 } from './RU_MUX2_1';
+import { IG_MUX2_1B } from './IG_MUX2_1B';
 
 export const initialEdges: Edge[] = [
 
-  ...PC_ADDR4, // PC to Instruction Memory and Adder 4 connections
-  
- 
-  { id: 'four->adder4',  source: 'four', target: 'adder4', targetHandle: 'fourTarget', type: 'smoothstep', animated: true,  markerEnd:
-    {
-      type: MarkerType.ArrowClosed,
-      width: 18,
-      height: 18,
-      color: '#FF0072',
-    },
-  },
-
+  ...PC_ADDR4, // PC to Instruction Memory to Adder 4 connections, and PC to Adder 4 connections
   ...IM_RU,  // Instruction Memory to Register Unit connections
   ...IM_CU, // Instruction Memory to Control Unit connections
 
   ...IM_IG, // Instruction Memory to Immediate Generator connections
-  { id: 'immSrc->immGenerator',  source: 'immSrc', target: 'immGenerator', targetHandle: 'immSrc', type: 'smoothstep', animated: true,  markerEnd:
-    {
-      type: MarkerType.ArrowClosed,
-      width: 18,
-      height: 18,
-      color: '#FF0072',
-    },
-  },
 
   ...RU_MUX2, // Register Unit to MUX 2 connections
+  ...IG_MUX2_1B, // Immediate Generator to MUX 2 connections and ImmSrc to ImmGen connections
 ];
--- client/simulatorView/src/components/svgs/section2/ImmGeneratosSvg.tsx
+++ client/simulatorView/src/components/svgs/section2/ImmGeneratosSvg.tsx
@@ -3,7 +3,7 @@ import { Handle, Position } from '@xyflow/react';
 
 export default function ImmGeneratorSvg() {
   return (
-    <div className='w-full'>
+    <div className='w-full '>
 
        <div className='relative w-full h-full'>
        <h2 className=" titleInElement top-[50%] left-[50%]  -translate-x-[50%] -translate-y-[50%] ">Inmmediate Generator</h2>
@@ -19,6 +19,10 @@ export default function ImmGeneratorSvg() {
                id="immSrc"
                position={Position.Left}
                style={{ top: '4.4rem', background: '#555', width: 10, height: 10 }} />
+
+        <Handle type="source"
+              position={Position.Right}
+              style={{   background: '#555', width: 10, height: 10 }} />
 
     </div>
     
