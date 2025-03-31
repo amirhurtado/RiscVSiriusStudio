f6691069ec64a7b3ccae8233f2008107b32d3c74
Amir <a.hurtado@utp.edu.co>
2025-03-30 20:23:26 -0500

    Add subtitles to the instruction memory and adjust CSS styles to improve presentation

--- client/simulatorView/src/components/svgs/section1/InstructionMemory.tsx
+++ client/simulatorView/src/components/svgs/section1/InstructionMemory.tsx
@@ -7,6 +7,15 @@ export function InstructionMemory() {
       <div className='relative w-full h-full'>
        <h2 className="titleInElement top-[15%] left-[50%]  -translate-x-[50%] -translate-y-[15%]">Instruction Memory</h2>
         <ContainerSVG height={12.6} />
+        
+        <div className='subtitleInElement top-[42%] left-[.8rem]'>
+          <h3 className=''>Address: </h3>
+        </div>
+        
+        <div className='subtitleInElement top-[63%] right-[.8rem]'>
+          <h3 >Instruction</h3>
+        </div>
+       
        </div>
       <Handle  type="target"
         position={Position.Left}
