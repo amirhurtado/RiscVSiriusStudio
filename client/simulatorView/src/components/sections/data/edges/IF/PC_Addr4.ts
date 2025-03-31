8dd33253566fbbefb29509647493cbf77a02de9a
Amir Hurtado <a.hurtado@utp.edu.co>
2025-03-31 08:02:51 -0500

    Isolate IF connections in their respective folder

--- client/simulatorView/src/components/sections/data/edges/IF/PC_Addr4.ts
+++ client/simulatorView/src/components/sections/data/edges/IF/PC_Addr4.ts
@@ -1,8 +1,8 @@
-// this file contains the edges for the PC_ADDR4 component AND FOUR_ADDR4 component
+// This file contains the connection between the program counter and the adder
 
 import { Edge, MarkerType  } from '@xyflow/react';
 
-export const PC_ADDR4: Edge[] = [
+export const PC_Addr4: Edge[] = [
    
      { id: 'pc->pivot1',  source: 'pc', target: 'pivot1', type: 'smoothstep', animated: true},
      { id: 'pivot1->adder4',  source: 'pivot1', target: 'adder4', targetHandle: 'pivot', type: 'smoothstep', animated: true,  markerEnd:
@@ -13,16 +13,5 @@ export const PC_ADDR4: Edge[] = [
          color: '#888888',
        }
      },
-
-
-     { id: 'four->adder4',  source: 'four', target: 'adder4', targetHandle: 'fourTarget', type: 'smoothstep', animated: true,  markerEnd:
-      {
-        type: MarkerType.ArrowClosed,
-        width: 18,
-        height: 18,
-        color: '#888888',
-      },
-    },
-
      
 ];
---
+++ client/simulatorView/src/components/sections/data/edges/IF/four_Addr4.ts
@@ -0,0 +1,14 @@
+// This file contains the connection between the constant 4 and the adder
+
+import { Edge, MarkerType  } from '@xyflow/react';
+
+export const four_ADDR4: Edge[] = [
+     { id: 'four->adder4',  source: 'four', target: 'adder4', targetHandle: 'fourTarget', type: 'smoothstep', animated: true,  markerEnd:
+      {
+        type: MarkerType.ArrowClosed,
+        width: 18,
+        height: 18,
+        color: '#888888',
+      },
+    },
+];
