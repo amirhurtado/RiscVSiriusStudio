1b1db0769a8c337bade1472a20c3d7d2983b908c
Amir Hurtado <a.hurtado@utp.edu.co>
2025-03-31 08:21:44 -0500

    Isolate IF-IE connections in their respective folder

--- client/simulatorView/src/components/sections/data/edges/initialEdges.ts
+++ client/simulatorView/src/components/sections/data/edges/initialEdges.ts
@@ -6,6 +6,7 @@
 import { IF } from './IF/IF';
 import { ID } from './ID/ID';
 import { IF_ID } from './IF-ID/IF-ID';
+import { IF_IE } from './IF-IE/IF-IE';
 
 import { Edge  } from '@xyflow/react';
 
@@ -13,7 +14,6 @@ import { PC_IM } from './IF/PC_IM';
 import { RU_MUXS } from './RU_MUXS';
 import { IG_MUXB } from './IG_MUXB';
 import { ALUXSRC } from './ALUXSRC';
-import { PC_MUXA } from './IF-IE/PC_MUXA';
 import { MUXS_ALU } from './MUXS_ALU';
 import { ALUOp_ALU } from './ALUOp_ALU';
 import { RU_BU } from './RU_BU';
@@ -27,6 +27,7 @@ export const initialEdges: Edge[] = [
   ...IF, // IF stage edges
   ...ID, // ID stage edges
   ...IF_ID, // IF-ID stage edges
+  ...IF_IE, // IF-IE stage edges
   
   ...PC_IM, // PC to Instruction Memory connection
 
@@ -36,7 +37,6 @@ export const initialEdges: Edge[] = [
 
   ...ALUXSRC, // ALU Source to MUX 2_1 A connections and ALU  Source to MUX 2_1 B connections 
 
-  ...PC_MUXA, // PC to MUX 2_1 A connections
   ...MUXS_ALU, // MUXS to ALU connections
 
   ...ALUOp_ALU, // ALUOp to ALU connection
