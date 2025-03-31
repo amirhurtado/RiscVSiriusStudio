77c5041d96070cc200e07d6c411339729334ebeb
Amir Hurtado <a.hurtado@utp.edu.co>
2025-03-31 08:35:56 -0500

    Isolate ID-IE connections in their respective folder V2

--- client/simulatorView/src/components/sections/data/edges/ID-IE/ID_IE.ts
+++ client/simulatorView/src/components/sections/data/edges/ID-IE/ID_IE.ts
@@ -3,7 +3,7 @@ import { IG_MUXB } from './IG_MUXB';
 import { RU_BU } from './RU_BU';
 import { RU_MUXS } from './RU_MUXS';
 
-export const IF_ID: Edge[] = [
+export const ID_IE: Edge[] = [
   ...IG_MUXB, // Immediate Generator to MUXB connection
   ...RU_BU, // Register Unit to Branch Unit connection
   ...RU_MUXS, // Register Unit to MUXS connection
