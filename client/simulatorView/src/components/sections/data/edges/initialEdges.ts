0c83719f24b3037dbe2f807c51059ebcbdfc2449
Amir Hurtado <a.hurtado@utp.edu.co>
2025-03-31 08:35:56 -0500

    Isolate ID-IE connections in their respective folder V2

--- client/simulatorView/src/components/sections/data/edges/initialEdges.ts
+++ client/simulatorView/src/components/sections/data/edges/initialEdges.ts
@@ -11,15 +11,11 @@ import { IF_IE } from './IF-IE/IF-IE';
 import { Edge  } from '@xyflow/react';
 
 import { PC_IM } from './IF/PC_IM';
-import { RU_MUXS } from './ID-IE/RU_MUXS';
-import { IG_MUXB } from './ID-IE/IG_MUXB';
 import { ALUXSRC } from './ALUXSRC';
 import { MUXS_ALU } from './MUXS_ALU';
 import { ALUOp_ALU } from './ALUOp_ALU';
-import { RU_BU } from './ID-IE/RU_BU';
 import { BrOp_BU } from './BrOp_BU';
-
-
+import { ID_IE } from './ID-IE/ID_IE';
 
 export const initialEdges: Edge[] = [
 
@@ -28,19 +24,14 @@ export const initialEdges: Edge[] = [
   ...ID, // ID stage edges
   ...IF_ID, // IF-ID stage edges
   ...IF_IE, // IF-IE stage edges
+  ...ID_IE, // ID-IE stage edges
   
   ...PC_IM, // PC to Instruction Memory connection
-
-
-  ...RU_MUXS, // Register Unit to MUXS connections
-  ...IG_MUXB, // Immediate Generator to MUXB connections and ImmSrc to ImmGen connections
 
   ...ALUXSRC, // ALU Source to MUX 2_1 A connections and ALU  Source to MUX 2_1 B connections 
 
   ...MUXS_ALU, // MUXS to ALU connections
 
   ...ALUOp_ALU, // ALUOp to ALU connection
-
-  ...RU_BU, // Register Unit to Branch Unit connections
   ...BrOp_BU // Branch operation to Branch Unit connections
 ];
