63a8112b2e3081bf2b680858b861c4db30a68be2
Amir Hurtado <a.hurtado@utp.edu.co>
2025-03-31 09:49:09 -0500

    Add DMWR and DMCtrl components with connections in MEM section

---
+++ client/simulatorView/src/components/elements/MEM/DMWr.tsx
@@ -0,0 +1,20 @@
+import { Handle, Position } from '@xyflow/react';
+
+export default function DMWR() {
+  return (
+    <div className='w-full'>
+
+       <div className='relative w-full h-full'>
+       <h2 className="titleInElement top-[-1.7rem] left-[50%] -translate-x-[50%] ">DMWR</h2>
+       </div>
+
+      <Handle  type="source"
+               position={Position.Bottom}
+               className='output'
+               style={{ top: '4.5rem'}} />
+
+        
+    </div>
+    
+  );
+}
