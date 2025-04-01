3777f465b4407b4eecf684d2fd572ad5f1b1f021
Amir <a.hurtado@utp.edu.co>
2025-04-01 15:33:13 -0500

    Clock is removed from data memory

--- client/simulatorView/src/components/elements/MEM/DataMemory.tsx
+++ client/simulatorView/src/components/elements/MEM/DataMemory.tsx
@@ -1,6 +1,5 @@
 import { Handle, Position } from '@xyflow/react';
 import ContainerSVG from '../ContainerSVG';
-import {Triangle} from 'lucide-react';
 
 export default function DataMemory() {
   return (
@@ -9,7 +8,6 @@ export default function DataMemory() {
        <div className='relative w-full h-full'>
        <h2 className="titleInElement top-[15%] left-[50%]  -translate-x-[50%] -translate-y-[15%] ">Data Memory</h2>
         <ContainerSVG height={18}  />
-        <Triangle size={24} className='absolute left-[50%]  transform -translate-x-[50%] text-[#404040] bottom-0 z-2' />
        </div>
 
        <Handle  type="target"
