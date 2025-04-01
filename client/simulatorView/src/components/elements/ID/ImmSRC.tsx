3da0706e1c4bf3239d6ca54cd180aa3505b09a8d
Amir Hurtado <a.hurtado@utp.edu.co>
2025-04-01 10:48:40 -0500

    Enhance ImmSrc component to utilize OverlayContext for dynamic styling based on overlay state

--- client/simulatorView/src/components/elements/ID/ImmSRC.tsx
+++ client/simulatorView/src/components/elements/ID/ImmSRC.tsx
@@ -1,16 +1,21 @@
 import { Handle, Position } from '@xyflow/react';
+import { useOverlay } from '@/context/OverlayContext';
 
 export default function ImmSrc() {
+  const { overlayDecodeActive} = useOverlay();
+
   return (
     <div className='w-full'>
 
        <div className='relative w-full h-full'>
-       <h2 className="titleInElement right-[1.7rem] top-[50%] -translate-y-[50%]  ">IMMSrc</h2>
+       <h2 className={`titleInElement right-[1.7rem] top-[50%] -translate-y-[50%]  ${overlayDecodeActive && 'overlay-scale'}`}>IMMSrc</h2>
        </div>
         
-       <Handle  type="source"
-        position={Position.Right}
-        className='output-tunnel' style={{top:'1.2rem'}} />
+       <div  className={`${overlayDecodeActive && 'overlay-rotate'}`} >
+        <Handle  type="source"
+          position={Position.Right}
+          className='output-tunnel' style={{top:'1.2rem'}} />
+        </div>
     </div>
     
   );
