854f03d134993c307b586022c21949bccdf5717e
Amir <a.hurtado@utp.edu.co>
2025-03-31 21:16:23 -0500

    Add RUWr component and establish connections in the ID section of the simulator

---
+++ client/simulatorView/src/components/elements/ID/RUWr.tsx
@@ -0,0 +1,17 @@
+import { Handle, Position } from '@xyflow/react';
+
+export default function RUWr() {
+  return (
+    <div className='w-full'>
+
+       <div className='relative w-full h-full'>
+       <h2 className="titleInElement top-[-.4rem] left-[50%] -translate-x-[50%]  ">RUWr</h2>
+       </div>
+        
+       <Handle  type="source"
+        position={Position.Top}
+        className='output'  />
+    </div>
+    
+  );
+}
--- client/simulatorView/src/components/sections/data/edges/ID/ID.ts
+++ client/simulatorView/src/components/sections/data/edges/ID/ID.ts
@@ -1,6 +1,8 @@
 import { Edge  } from '@xyflow/react';
 import { immSrc_IG } from './immSrc_IG';
+import { RUWr_RU } from './RUWr_RU';
 
 export const ID: Edge[] = [
   ...immSrc_IG, // Immediate Source to Immediate Generator connection
+  ...RUWr_RU, // Register Unit Write to Register Unit connection
 ]
