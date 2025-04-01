628f5631c66a60c3bfb342adcf911de1dc3df446
Amir Hurtado <a.hurtado@utp.edu.co>
2025-04-01 11:04:21 -0500

    Add overlayExecute state management to enhance ExecuteTunnel and ALUASrc interactions

--- client/simulatorView/src/components/elements/IE/ALUASrc.tsx
+++ client/simulatorView/src/components/elements/IE/ALUASrc.tsx
@@ -1,17 +1,23 @@
 import { Handle, Position } from '@xyflow/react';
+import { useOverlay } from '@/context/OverlayContext';
+
 
 export default function ALUASrc() {
+    const { overlayExecuteActive} = useOverlay();
+  
   return (
     <div className='w-full'>
 
        <div className='relative w-full h-full'>
-       <h2 className="titleInElement top-[-1.7rem] left-[50%] -translate-x-[50%] ">ALUASrc</h2>
+       <h2 className={` titleInElement top-[-.8rem]   ${overlayExecuteActive && 'overlay-scale'}`}>ALUASrc</h2>
        </div>
 
+       <div  className={`${overlayExecuteActive && 'overlay-rotate'}`} >
       <Handle  type="source"
                position={Position.Bottom}
                className='output-tunnel'
-               style={{ top: '4.5rem'}} />
+               style={{ top: '2rem'}} />
+      </div>
 
         
     </div>
--- client/simulatorView/src/context/OverlayContext.tsx
+++ client/simulatorView/src/context/OverlayContext.tsx
@@ -4,20 +4,27 @@ import { createContext, useContext, useState, ReactNode } from "react";
 type OverlayContextType = {
   overlayDecodeActive: boolean;
   setOverlayDecodeActive: (active: boolean) => void;
+
+  overlayExecuteActive: boolean;
+  setOverlayExecuteActive: (active: boolean) => void;
 };
 
 const OverlayContext = createContext<OverlayContextType>({
     overlayDecodeActive: false,
   setOverlayDecodeActive: () => {},
+
+  overlayExecuteActive: false,
+  setOverlayExecuteActive: () => {},
 });
 
 export const useOverlay = () => useContext(OverlayContext);
 
 export const OverlayProvider = ({ children }: { children: ReactNode }) => {
   const [overlayDecodeActive, setOverlayDecodeActive] = useState(false);
+  const [overlayExecuteActive, setOverlayExecuteActive] = useState(false);
 
   return (
-    <OverlayContext.Provider value={{ overlayDecodeActive, setOverlayDecodeActive }}>
+    <OverlayContext.Provider value={{ overlayDecodeActive, setOverlayDecodeActive, overlayExecuteActive, setOverlayExecuteActive }}>
       {children}
     </OverlayContext.Provider>
   );
