dade5fe53fb6415bfd880350df8354776cb86b8b
Amir <a.hurtado@utp.edu.co>
2025-04-01 17:54:19 -0500

    Add MuxD connection to PC and adjust MuxD node position

---
+++ client/simulatorView/src/components/sections/data/edges/IF/MUXD_PC.ts
@@ -0,0 +1,12 @@
+import { Edge, MarkerType  } from '@xyflow/react';
+
+export const MUXD_PC: Edge[] = [
+    { id: 'muxD->pc',  source: 'muxD', target: 'pc', type: 'smoothstep', animated: true,  markerEnd:
+        {
+         type: MarkerType.ArrowClosed,
+         width: 18,
+         height: 18,
+         color: '#888888',
+       },
+     },
+];
