c1b9a36651a521df796e5e9b8342955acd175c4a
Amir <a.hurtado@utp.edu.co>
2025-04-01 16:28:20 -0500

    Add the MuxD component

---
+++ client/simulatorView/src/components/elements/IF/MuxD.tsx
@@ -0,0 +1,41 @@
+import MuxContainer from "../MUXContainer";
+import { Handle, Position } from '@xyflow/react';
+
+function MuxD() {
+  return (
+    <div className='relative w-full h-full'>
+      <div className="relative w-full h-full">
+        <MuxContainer />
+      </div>
+      
+      
+      <Handle  type="target"
+              id="pc"
+              position={Position.Left}
+              className='input'
+              style={{ top: '2.8rem' }} />
+  
+      <Handle  type="target"
+              id="registersUnitA"
+              position={Position.Left}
+              className='input'
+              style={{ top: '6.8rem' }} />
+
+      <Handle  type="target"
+              id="aluASrc"
+              position={Position.Top}
+              className='input'
+              style={{ top: '1.5rem' }} />
+
+      <Handle  type="source"
+            position={Position.Right}
+            className='output'
+            style={{right:'.8rem' }} />
+
+
+     
+    </div>
+  );
+}
+
+export default MuxD;
