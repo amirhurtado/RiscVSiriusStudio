d63e68c9a42ed7d89fee88a0590d0c090d24a782
Amir Hurtado <a.hurtado@utp.edu.co>
2025-04-01 11:04:21 -0500

    Add overlayExecute state management to enhance ExecuteTunnel and ALUASrc interactions

--- client/simulatorView/src/components/elements/ID/ControlUnit/outputTunnels/ExecuteTunnel.tsx
+++ client/simulatorView/src/components/elements/ID/ControlUnit/outputTunnels/ExecuteTunnel.tsx
@@ -1,8 +1,15 @@
 import TunnelContainer from "./TunnelContainer"
+import { useOverlay } from "@/context/OverlayContext";
+
 
 const ExecuteTunnel = () => {
+  const { setOverlayExecuteActive } = useOverlay();
+
   return (
-    <div className="w-full relative">
+    <div className="w-full relative"
+    onMouseEnter={() => setOverlayExecuteActive(true)}
+    onMouseLeave={() => setOverlayExecuteActive(false)}
+    >
      <h2 className="subtitleInTunnel ">
        Execute
       </h2>
