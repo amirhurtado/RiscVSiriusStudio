0147c087b71a7a9688ad5ffc083ca2ba1af525c3
Amir <a.hurtado@utp.edu.co>
2025-04-01 13:23:18 -0500

    Adjust styles and positions on the ALUOp, DMCtrl, and DMWR components to improve alignment and animation in the overlay state

--- client/simulatorView/src/components/elements/MEM/DMCtrl.tsx
+++ client/simulatorView/src/components/elements/MEM/DMCtrl.tsx
@@ -1,17 +1,23 @@
 import { Handle, Position } from '@xyflow/react';
+import { useOverlay } from '@/context/OverlayContext';
+
 
 export default function DMCtrl() {
+  const { overlayMemoryActive} = useOverlay();
+
   return (
     <div className='w-full'>
 
        <div className='relative w-full h-full'>
-       <h2 className="titleInElement top-[-1.7rem] left-[50%] -translate-x-[50%] ">DMCtrl</h2>
+       <h2 className={`titleInElement  top-[-1.3rem] left-[50%] -translate-x-[50%]   ${overlayMemoryActive && 'overlay-scale'}`}>DMCtrl</h2>
        </div>
-
+       
+       <div  className={`${overlayMemoryActive && 'overlay-moving-t'}`} >
       <Handle  type="source"
                position={Position.Bottom}
                className='output-tunnel'
-               style={{ top: '4.5rem'}} />
+               style={{ top: '1rem'}} />
+      </div>
 
         
     </div>
