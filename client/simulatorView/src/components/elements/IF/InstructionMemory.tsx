62ab540e054d0b54b893c29c265bdbac11a3278b
Amir Hurtado <a.hurtado@utp.edu.co>
2025-04-02 12:39:01 -0500

    Adjust bg to pivots to ensure that there are fewer gaps between connections

--- client/simulatorView/src/components/elements/IF/InstructionMemory.tsx
+++ client/simulatorView/src/components/elements/IF/InstructionMemory.tsx
@@ -23,7 +23,7 @@ export function InstructionMemory() {
       <Handle  type="source"
         position={Position.Right}
         className='output'
-        style={{ top: '10rem'}} />
+        style={{ top: '10.1rem'}} />
       </div>
   );
 }
