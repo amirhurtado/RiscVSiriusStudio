647fc4f07b36aa6f4478b78c865f4ae1911230b7
Amir Hurtado <a.hurtado@utp.edu.co>
2025-04-02 15:22:23 -0500

    PC line animation to its connections

--- client/simulatorView/src/components/animateLine/animateLine.ts
+++ client/simulatorView/src/components/animateLine/animateLine.ts
@@ -1,21 +1,31 @@
 import { Edge } from '@xyflow/react';
 
+const pcToX = new Set([
+  'pc->pivot1',
+  'pc->instMemory',
+  'pivot1->pivotJump1',
+  'pivotJump1->pivotJump2',
+  'pivotJump2->pivotJump3',
+  'pivotJump3->muxA'
+]);
+
 export const animateLine = (
   updateEdge: (id: string, newEdge: Partial<Edge>) => void,
   edge: Edge,
   animated: boolean = true
 ): void => {
 
+  console.log('edge', edge);
 
-  console.log(edge.id)
-  //
-  if(edge.id === 'pc->pivot1' ){
+  if (pcToX.has(edge.id)) {
+    updateEdge('pc->pivot1', { animated });
+    updateEdge('pc->instMemory', { animated });
+    updateEdge('pivot1->adder4', { animated });
+    updateEdge('pivot1->pivotJump1', { animated });
+    updateEdge('pivotJump1->pivotJump2', { animated });
+    updateEdge('pivotJump2->pivotJump3', { animated });
+    updateEdge('pivotJump3->muxA', { animated });
+  } else {
     updateEdge(edge.id, { animated });
-    updateEdge('pc->instMemory', { animated })
-    updateEdge('pivot1->adder4', { animated })
   }
-  else{
-    updateEdge(edge.id, { animated });
-  }
-  
 };
