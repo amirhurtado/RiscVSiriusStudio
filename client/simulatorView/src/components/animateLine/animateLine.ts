d534b727a0ea1aadd4de6890dffc30a6241a663d
Amir Hurtado <a.hurtado@utp.edu.co>
2025-04-02 21:47:55 -0500

    Add instruction memory edge groups for animation handling

--- client/simulatorView/src/components/animateLine/animateLine.ts
+++ client/simulatorView/src/components/animateLine/animateLine.ts
@@ -1,5 +1,6 @@
 import { Edge } from '@xyflow/react';
 
+// Definición de rutas comunes
 const pivotJumpGroup = [
   'pc->pivot1', 
   'pivot1->pivotJump1', 
@@ -8,7 +9,19 @@ const pivotJumpGroup = [
   'pivotJump3->muxA'
 ];
 
+const instructionMemoryGroup = [
+  'instructionMemory->pivot3',  
+  'pivot3->immediateGenerator[31:7]', 
+  'pivot3->RegistersUnit[11:7]', 
+  'pivot3->RegistersUnit[24:20]', 
+  'pivot3->RegistersUnit[19:15]', 
+  'pivot3->controlUnit[35:25]', 
+  'pivot3->controlUnit[14:12]', 
+  'pivot3->controlUnit[6:0]'
+];
+
 const edgeGroups: Record<string, string[]> = {
+  // Grupo IF
   'pc->instMemory': ['pc->instMemory'],
   'pc->pivot1': ['pc->instMemory', 'pc->pivot1', 'pivot1->adder4', ...pivotJumpGroup],
   'pivot1->adder4': ['pc->pivot1', 'pivot1->adder4'],
@@ -16,7 +29,16 @@ const edgeGroups: Record<string, string[]> = {
   'pivotJump1->pivotJump2': pivotJumpGroup,
   'pivotJump2->pivotJump3': pivotJumpGroup,
   'pivotJump3->muxA': pivotJumpGroup,
-  'four->adder4': ['four->adder4']
+  'four->adder4': ['four->adder4'],
+
+  'instructionMemory->pivot3': instructionMemoryGroup,
+  'pivot3->immediateGenerator[31:7]': ['instructionMemory->pivot3', 'pivot3->immediateGenerator[31:7]'],
+  'pivot3->RegistersUnit[11:7]': ['instructionMemory->pivot3', 'pivot3->RegistersUnit[11:7]'],
+  'pivot3->RegistersUnit[24:20]': ['instructionMemory->pivot3', 'pivot3->RegistersUnit[24:20]'],
+  'pivot3->RegistersUnit[19:15]': ['instructionMemory->pivot3', 'pivot3->RegistersUnit[19:15]'],
+  'pivot3->controlUnit[35:25]': ['instructionMemory->pivot3', 'pivot3->controlUnit[35:25]'],
+  'pivot3->controlUnit[14:12]': ['instructionMemory->pivot3', 'pivot3->controlUnit[14:12]'],
+  'pivot3->controlUnit[6:0]': ['instructionMemory->pivot3', 'pivot3->controlUnit[6:0]'],
 };
 
 export const animateLine = (
@@ -27,5 +49,6 @@ export const animateLine = (
   console.log('edge', edge);
 
   const idsToUpdate = edgeGroups[edge.id];
+
   idsToUpdate.forEach(id => updateEdge(id, { animated }));
 };
