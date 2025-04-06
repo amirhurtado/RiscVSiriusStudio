d1b7568a6d79531fc86922bbd2ffacda244d735a
Amir <a.hurtado@utp.edu.co>
2025-04-06 13:57:46 -0500

    Recover the initial style of each edge when it moves to the next instruction

--- client/panelView/src/components/graphic/Canva/InstructionEffect.tsx
+++ client/panelView/src/components/graphic/Canva/InstructionEffect.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { Edge } from '@xyflow/react';
 import { useIR } from '@/context/graphic/IRContext';
 import { useMemoryTable } from '@/context/panel/MemoryTableContext';
@@ -10,32 +10,31 @@ interface InstructionEffectProps {
 const InstructionEffect: React.FC<InstructionEffectProps> = ({ setEdges }) => {
   const { ir, setCurrentType } = useIR();
   const { newPc } = useMemoryTable();
+  
+  const prevTargetEdgesRef = useRef<string[]>([]);
 
   useEffect(() => {
     let targetEdges: string[] = [];
-
     switch (ir.instructions[newPc].type) {
       case "R": {
         console.log("R", ir.instructions[newPc]);
         setCurrentType("R");
         targetEdges = [
-          
-          //MUX A
+          // MUX A
           'pivot1->pivotJump1',
           'pivotJump1->pivotJump2',
           'pivotJump2->pivotJump3',
           'pivotJump3->muxA',
-          //NO IMM
+          // No IMM
           'pivot3->immediateGenerator[31:7]',
           'immSrc->immGenerator',
           'immGenerator->pivot10',
-          //MUX B
+          // MUX B
           'pivot10->muxB',
-
-          //NO BRANCHUNIT
+          // No BranchUnit
           'pivot2->branchUnit',
           'pivot4->branchUnit',
-          //NO MEM
+          // No MEM
           'pivot2->pivot5',
           'pivot5->pivotJump5',
           'pivotJump5->pivot6',
@@ -43,62 +42,81 @@ const InstructionEffect: React.FC<InstructionEffectProps> = ({ setEdges }) => {
           'pivot7->dataMemory',
           'dmWr->dataMemory',
           'dmCtrl->dataMemory',
-          
-          //MUX C
+          // MUX C
           'dataMemory->muxC',
-
+          // MUX D
           'pivot18->pivotJump8',
           'pivotJump8->pivotJump9',
           'pivotJump9->pivot13',
           'pivot13->muxC',
-
-          //MUXD
           'pivot7->pivot16',
           'pivot16->pivot17',
           'pivot17->muxD',
-
         ];
         break;
       }
-      case "I":
+      case "I": {
         console.log("I", ir.instructions[newPc]);
         setCurrentType("I");
         break;
-      case "S":
+      }
+      case "S": {
         console.log("S", ir.instructions[newPc]);
         setCurrentType("S");
         break;
-      case "B":
+      }
+      case "B": {
         console.log("B", ir.instructions[newPc]);
         setCurrentType("B");
         break;
-      case "J":
+      }
+      case "J": {
         console.log("J", ir.instructions[newPc]);
         setCurrentType("J");
         break;
-      case "U":
+      }
+      case "U": {
         console.log("U", ir.instructions[newPc]);
         setCurrentType("U");
         break;
+      }
       default:
         break;
     }
 
-    setEdges((prevEdges: Edge[]) =>
-      prevEdges.map((edge) => {
-        if (targetEdges.includes(edge.id)) {
+    setEdges(prevEdges => {
+      
+      const resetEdges = prevEdges.map(edge => {
+        if (prevTargetEdgesRef.current.includes(edge.id) && !targetEdges.includes(edge.id)) {
           return {
             ...edge,
-            style: {
+            disabled: false,
+            style: { 
               ...edge.style,
-              stroke: '#D3D3D3'
-            },
-            disabled: true 
+              stroke: "#3B59B6" 
+            }
           };
         }
         return edge;
-      })
-    );
+      });
+      const newEdges = resetEdges.map(edge => {
+        if (targetEdges.includes(edge.id)) {
+          return {
+            ...edge,
+            disabled: true,
+            style: {
+              ...edge.style,
+              stroke: "#D3D3D3"
+            }
+          };
+        }
+        return edge;
+      });
+
+      return newEdges;
+    });
+
+    prevTargetEdgesRef.current = targetEdges;
   }, [newPc, ir, setCurrentType, setEdges]);
 
   return null;
