68139e35a0d4de9f5aaa9907fb2dcabf1850e99f
Amir <a.hurtado@utp.edu.co>
2025-04-06 16:52:41 -0500

    Code refactoring

--- client/panelView/src/components/graphic/Canva/InstructionEffect.tsx
+++ client/panelView/src/components/graphic/Canva/InstructionEffect.tsx
@@ -7,6 +7,82 @@ interface InstructionEffectProps {
   setEdges: React.Dispatch<React.SetStateAction<Edge[]>>;
 }
 
+// COMUN EDGES
+const MUX_A = [
+  'pivot1->pivotJump1',
+  'pivotJump1->pivotJump2',
+  'pivotJump2->pivotJump3',
+  'pivotJump3->muxA'
+];
+
+const NO_IMM = [
+  'pivot3->immediateGenerator[31:7]',
+  'immSrc->immGenerator',
+  'immGenerator->pivot10'
+];
+
+const MUX_B_R = ['pivot10->muxB'];
+const MUX_B_I = ['registersUnit->pivot2', 'pivot2->muxB'];
+
+const NO_BRANCH_UNIT = [
+  'pivot2->branchUnit',
+  'pivot4->branchUnit'
+];
+
+const NO_MEM_R = [
+  'pivot2->pivot5',
+  'pivot5->pivotJump5',
+  'pivotJump5->pivot6',
+  'pivot6->dataMemory',
+  'pivot7->dataMemory',
+  'dmWr->dataMemory',
+  'dmCtrl->dataMemory'
+];
+
+const MUX_C_R = [
+  'dataMemory->muxC',
+  'pivot18->pivotJump8',
+  'pivotJump8->pivotJump9',
+  'pivotJump9->pivot13',
+  'pivot13->muxC'
+];
+
+const MUX_D_R = [
+  'pivot7->pivot16',
+  'pivot16->pivot17',
+  'pivot17->muxD'
+];
+
+// I
+const NO_FUNT7 = ['pivot22->controlUnit[31:25]'];
+const NO_RS2 = ['pivot20->RegistersUnit[24:20]'];
+
+const MUX_C_L_EXTRA = [
+  'pivot7->pivot8',
+  'pivot8->pivotJump6',
+  'pivotJump6->pivot9',
+  'pivot9->muxC'
+];
+
+const NO_RS2_DM = [
+  'pivot2->pivot5',
+  'pivot5->pivotJump5',
+  'pivotJump5->pivot6',
+  'pivot6->dataMemory'
+];
+
+const MUX_D_JALR = ['pivot18->pivot19', 'pivot19->muxD'];
+
+const MUX_C_JALR_EXTRA = [
+  'dataMemory->muxC',
+  'pivot7->pivot8',
+  'pivot8->pivotJump6',
+  'pivotJump6->pivot9',
+  'pivot9->muxC'
+];
+
+const NO_MEM_FULL = NO_MEM_R; // Assuming this is the same as NO_MEM_R
+
 const InstructionEffect: React.FC<InstructionEffectProps> = ({ setEdges }) => {
   const { ir, setCurrentType } = useIR();
   const { newPc } = useMemoryTable();
@@ -15,163 +91,79 @@ const InstructionEffect: React.FC<InstructionEffectProps> = ({ setEdges }) => {
 
   useEffect(() => {
     let targetEdges: string[] = [];
-    switch (ir.instructions[newPc].type) {
-      case "R": {
-        console.log("R", ir.instructions[newPc]);
+    const currentInstruction = ir.instructions[newPc];
+    console.log(currentInstruction.type, currentInstruction);
+    
+    switch (currentInstruction.type) {
+      case "R":
         setCurrentType("R");
         targetEdges = [
-          // MUX A
-          'pivot1->pivotJump1',
-          'pivotJump1->pivotJump2',
-          'pivotJump2->pivotJump3',
-          'pivotJump3->muxA',
-          // No IMM
-          'pivot3->immediateGenerator[31:7]',
-          'immSrc->immGenerator',
-          'immGenerator->pivot10',
-          // MUX B
-          'pivot10->muxB',
-          // No BranchUnit
-          'pivot2->branchUnit',
-          'pivot4->branchUnit',
-          // No MEM
-          'pivot2->pivot5',
-          'pivot5->pivotJump5',
-          'pivotJump5->pivot6',
-          'pivot6->dataMemory',
-          'pivot7->dataMemory',
-          'dmWr->dataMemory',
-          'dmCtrl->dataMemory',
-          // MUX C
-          'dataMemory->muxC',
-          'pivot18->pivotJump8',
-          'pivotJump8->pivotJump9',
-          'pivotJump9->pivot13',
-          'pivot13->muxC',
-          // MUX D
-          'pivot7->pivot16',
-          'pivot16->pivot17',
-          'pivot17->muxD',
+          ...MUX_A,
+          ...NO_IMM,
+          ...MUX_B_R,
+          ...NO_BRANCH_UNIT,
+          ...NO_MEM_R,
+          ...MUX_C_R,
+          ...MUX_D_R,
         ];
         break;
-      }
-      case "I": {
-        console.log("I", ir.instructions[newPc]);
-        if(ir.instructions[newPc].opcode === "0010011"){
+      case "I":
+        if (currentInstruction.opcode === "0010011") {
           setCurrentType("I");
           targetEdges = [
-            // NO FUNT7
-            'pivot22->controlUnit[31:25]',
-            //NO RS2
-            'pivot20->RegistersUnit[24:20]',
-  
-            //MUXA
-            'pivot1->pivotJump1',
-            'pivotJump1->pivotJump2',
-            'pivotJump2->pivotJump3',
-            'pivotJump3->muxA',
-            //MUXB
-            'registersUnit->pivot2',
-            'pivot2->muxB',
-            // MUX C
-            'dataMemory->muxC',
-            'pivot18->pivotJump8',
-            'pivotJump8->pivotJump9',
-            'pivotJump9->pivot13',
-            'pivot13->muxC',
-  
-            // MUX D
-            'pivot7->pivot16',
-            'pivot16->pivot17',
-            'pivot17->muxD',
-  
-             // No BranchUnit
-             'pivot2->branchUnit',
-             'pivot4->branchUnit',
-  
-             // No MEM
-            'pivot2->pivot5',
-            'pivot5->pivotJump5',
-            'pivotJump5->pivot6',
-            'pivot6->dataMemory',
-            'pivot7->dataMemory',
-            'dmWr->dataMemory',
-            'dmCtrl->dataMemory',   
+            ...NO_FUNT7,
+            ...NO_RS2,
+            ...MUX_A,
+            ...MUX_B_I,
+            ...MUX_C_R,
+            ...MUX_D_R,
+            ...NO_BRANCH_UNIT,
+            ...NO_MEM_R,
           ];
-        }else if(ir.instructions[newPc].opcode === "0000011"){
-          setCurrentType('L');
+        } else if (currentInstruction.opcode === "0000011") {
+          setCurrentType("L");
           targetEdges = [
-            // NO FUNT7
-            'pivot22->controlUnit[31:25]',
-            //NO RS2
-            'pivot20->RegistersUnit[24:20]',
-  
-            //MUXA
-            'pivot1->pivotJump1',
-            'pivotJump1->pivotJump2',
-            'pivotJump2->pivotJump3',
-            'pivotJump3->muxA',
-            //MUXB
-            'registersUnit->pivot2',
-            'pivot2->muxB',
-
-            // MUX C
-            'pivot18->pivotJump8',
-            'pivotJump8->pivotJump9',
-            'pivotJump9->pivot13',
-            'pivot13->muxC',
-
-            'pivot7->pivot8',
-            'pivot8->pivotJump6',
-            'pivotJump6->pivot9',
-            'pivot9->muxC',
-  
-            // MUX D
-            'pivot7->pivot16',
-            'pivot16->pivot17',
-            'pivot17->muxD',
-  
-             // No BranchUnit
-             'pivot2->branchUnit',
-             'pivot4->branchUnit',
-
-             //NO RS2-DM
-             'pivot2->pivot5',
-             'pivot5->pivotJump5',
-             'pivotJump5->pivot6',
-             'pivot6->dataMemory',
-  
+            ...NO_FUNT7,
+            ...NO_RS2,
+            ...MUX_A,
+            ...MUX_B_I,
+            ...MUX_C_R.slice(1),
+            ...MUX_C_L_EXTRA,
+            ...MUX_D_R,
+            ...NO_BRANCH_UNIT,
+            ...NO_RS2_DM,
+          ];
+        } else if (currentInstruction.opcode === "1100111") {
+          setCurrentType("JALR");
+          targetEdges = [
+            ...NO_FUNT7,
+            ...NO_RS2,
+            ...MUX_A,
+            ...MUX_B_I,
+            ...MUX_C_JALR_EXTRA,
+            ...MUX_D_JALR,
+            ...NO_BRANCH_UNIT,
+            ...NO_MEM_FULL,
           ];
         }
-        
         break;
-      }
-      case "S": {
-        console.log("S", ir.instructions[newPc]);
+      case "S":
         setCurrentType("S");
         break;
-      }
-      case "B": {
-        console.log("B", ir.instructions[newPc]);
+      case "B":
         setCurrentType("B");
         break;
-      }
-      case "J": {
-        console.log("J", ir.instructions[newPc]);
+      case "J":
         setCurrentType("J");
         break;
-      }
-      case "U": {
-        console.log("U", ir.instructions[newPc]);
+      case "U":
         setCurrentType("U");
         break;
-      }
       default:
         break;
     }
 
     setEdges(prevEdges => {
-      
       const resetEdges = prevEdges.map(edge => {
         if (prevTargetEdgesRef.current.includes(edge.id) && !targetEdges.includes(edge.id)) {
           return {
--- client/panelView/src/components/graphic/elements/IE/BranchUnit.tsx
+++ client/panelView/src/components/graphic/elements/IE/BranchUnit.tsx
@@ -1,15 +1,13 @@
 import { Handle, Position } from '@xyflow/react';
-import { useIR } from '@/context/graphic/IRContext';
 import ContainerSVG from '../ContainerSVG';
 
 export default function BranchUnit() {
-  const { currentType} = useIR();
   return (
     <div className='w-full'>
 
        <div className='relative w-full h-full'>
        <h2 className=" titleInElement top-[50%] left-[50%]  -translate-x-[50%] -translate-y-[50%] ">Branch Unit</h2>
-        <ContainerSVG height={7.2} active={currentType === "B"}  />
+        <ContainerSVG height={7.2} active={true}  />
        </div>
 
        <Handle  type="target"
