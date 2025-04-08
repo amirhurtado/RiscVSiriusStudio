8b0656cc3087f8763a9923d80235c3c08a1cbef6
Amir <a.hurtado@utp.edu.co>
2025-04-08 12:23:31 -0500

    Dynamic instruction hex value in IM

--- client/panelView/src/components/graphic/elements/IF/InstructionMemory/LabelValueContainer.tsx
+++ client/panelView/src/components/graphic/elements/IF/InstructionMemory/LabelValueContainer.tsx
@@ -1,10 +1,20 @@
+import { useState, useEffect } from 'react';
+
 import LabelValue from '@/components/graphic/LabelValue';
+import { useIR } from '@/context/graphic/IRContext';
 import { usePC } from '@/context/shared/PCCONTEXT';
-import { useFormattedPC } from '@/hooks/graphic/useFormattedPC'; 
+import { useFormattedPC } from '@/hooks/graphic/useFormattedPC';
 
 const LabelValueContainer = () => {
   const { newPc } = usePC();
   const formattedPC = useFormattedPC(newPc);
+
+  const { ir } = useIR();
+  const [hexEncoding, setHexEncoding] = useState("00-00-00-00");
+
+  useEffect(() => {
+    setHexEncoding(ir.instructions[newPc].encoding.hexEncoding);
+  }, [newPc, ir])
 
   return (
     <>
@@ -13,7 +23,7 @@ const LabelValueContainer = () => {
       </div>
 
       <div className='absolute top-[8rem] right-[.8rem]'>
-        <LabelValue label="Instruction" value="h'00-00-00-00" input={false} />
+        <LabelValue label="Instruction" value={`h'${hexEncoding}`} input={false} />
       </div>
     </>
   );
