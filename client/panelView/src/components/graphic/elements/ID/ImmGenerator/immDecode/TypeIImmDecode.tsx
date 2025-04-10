ee14a72a9430520a7f29ec47235dbece1014c7ff
Amir Hurtado <a.hurtado@utp.edu.co>
2025-04-10 17:25:57 -0500

    Update asset references in panelView index.html for new build outputs V2

--- client/panelView/src/components/graphic/elements/ID/ImmGenerator/immDecode/TypeIImmDecode.tsx
+++ client/panelView/src/components/graphic/elements/ID/ImmGenerator/immDecode/TypeIImmDecode.tsx
@@ -11,23 +11,23 @@ const TypeIImmDecode = () => {
 
 
   return (
-    <div className="space-y-1  w-full max-h-[30rem] overflow-auto hide-scrollbar font-mono">
+    <div className="space-y-1  w-full max-h-[30rem] text-[.75rem] text-black overflow-auto hide-scrollbar font-mono">
             <img src='immTypeIDecodeSvg.svg' alt="immDecode" height={100} width={100} className='w-full h-full rounded-md' /> 
-            <div className='absolute text-[.75rem] text-black flex gap-[.79rem] top-[2.5rem] left-[1.1rem]'>
+            <div className='absolute flex gap-[.79rem] top-[2.5rem] left-[1.1rem]'>
               {Array.from(data).slice(0, 4).map((item, index) => (
                   <p key={index}>{item}</p>
               ))
               }
             </div> 
 
-            <div className='absolute text-[.75rem] text-black flex gap-[.79rem] top-[2.5rem] left-[6.1rem]'>
+            <div className='absolute  flex gap-[.79rem] top-[2.5rem] left-[6.1rem]'>
               {Array.from(data).slice(4, 8).map((item, index) => (
                   <p key={index}>{item}</p>
               ))
               }
             </div>
 
-            <div className='absolute text-[.75rem] text-black flex gap-[.79rem] top-[2.5rem] left-[11rem]'>
+            <div className='absolute  flex gap-[.79rem] top-[2.5rem] left-[11rem]'>
               {Array.from(data).slice(8, 12).map((item, index) => (
                   <p key={index}>{item}</p>
               ))
@@ -35,9 +35,8 @@ const TypeIImmDecode = () => {
             </div>
             
                   
-            <div className='absolute text-[.75rem] text-black flex gap-[.83rem] bottom-[1.7rem] right-[4.97rem]'>
 
-            <div className='flex gap-[.79rem]'>
+            {/* <div className='absolute  bottom-[1.7rem] flex gap-[.79rem]'>
               {Array.from({ length: 20 }).map((_, index) => (
                   <p key={index}>{data[0]}</p>
               ))}
@@ -48,16 +47,15 @@ const TypeIImmDecode = () => {
                     <p key={index}>{item}</p>
                   ))
                 }
-              </div>
+              </div> */}
 
-              <div className='flex gap-[.815rem]'>
+              <div className='flex absolute  bottom-[1.7rem] right-[5rem] gap-[.79rem]'>
                 {Array.from(data).slice(5, 12).map((item, index) => (
                     <p key={index}>{item}</p>
                   ))
                 }
               </div>
             </div>    
-      </div>
   )
 }
 
