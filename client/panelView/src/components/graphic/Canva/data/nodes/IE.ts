15577849497c9ce9fbfdbacc7f87e27c7a6aedd1
Gustavo Gutiérrez-Sabogal <gustavo.gutierrez@utp.edu.co>
2025-04-11 15:23:19 -0500

    UI improvements

--- client/panelView/src/components/graphic/Canva/data/nodes/IE.ts
+++ client/panelView/src/components/graphic/Canva/data/nodes/IE.ts
@@ -3,286 +3,286 @@
 */
 
 import {
-    Node,
-  } from '@xyflow/react';
+  Node,
+} from '@xyflow/react';
 
 
 export const IE: Node[] = [
-    {
-        id: 'IE',
-        type: 'group',
-        data: { label: 'Section 3' },
-        position: { x: 920 + 730, y: 0 },
-        draggable: false,
-        zIndex: 0,
-        style: {
-          width: 680,
-          height: 1375,
-          backgroundColor: '#E3F2FD',
-          border: '1px solid #E3F2FD',
-          borderRadius: 8,
-        },
+  {
+    id: 'IE',
+    type: 'group',
+    data: { label: 'Section 3' },
+    position: { x: 920 + 730, y: 0 },
+    draggable: false,
+    zIndex: 0,
+    style: {
+      width: 680,
+      height: 1375,
+      backgroundColor: '#E3F2FD',
+      border: '1px solid #E3F2FD',
+      borderRadius: 8,
     },
+  },
 
-    {
-      id: 'title-IE',
-      type: 'title',
-      data: { label: 'Execute (IE)' },
-      position: { x: 0, y: 0 },
-      parentId: 'IE',
-      extent: 'parent',
-      draggable: false,
-      style: {
-        width: 900,
-        height: 50,
-        backgroundColor: 'transparent',
-        border: 'none',
-        borderRadius: 0,
-        padding: 0,
-        boxShadow: 'none',
-      },
+  {
+    id: 'title-IE',
+    type: 'title',
+    data: { label: 'Execute (IE)' },
+    position: { x: 0, y: 0 },
+    parentId: 'IE',
+    extent: 'parent',
+    draggable: false,
+    style: {
+      width: 680,
+      height: 50,
+      backgroundColor: 'transparent',
+      border: 'none',
+      borderRadius: 0,
+      padding: 0,
+      boxShadow: 'none',
     },
+  },
 
-    {
-      id: 'muxA',
-      type: 'muxA',
-      data: { label: 'MUX A' },
-      position: { x: 165, y: 625 },
-      parentId: 'IE',
-      extent: 'parent',
-      style: {
-        width: 65,
-        height: 150,
-        backgroundColor: 'transparent',
-        border: 'none',
-        borderRadius: 0,
-        padding: 0,
-        boxShadow: 'none',
-      },
+  {
+    id: 'muxA',
+    type: 'muxA',
+    data: { label: 'MUX A' },
+    position: { x: 165, y: 625 },
+    parentId: 'IE',
+    extent: 'parent',
+    style: {
+      width: 65,
+      height: 150,
+      backgroundColor: 'transparent',
+      border: 'none',
+      borderRadius: 0,
+      padding: 0,
+      boxShadow: 'none',
     },
-    
-    {
-      id: 'aluASrc',
-      type: 'aluASrc',
-      data: { label: 'ALU A SRC' },
-      position: { x: 153, y: 555 },
-      parentId: 'IE',
-      extent: 'parent',
-      style: {
-        width: 90,
-        height: 30,
-        backgroundColor: 'transparent',
-        border: 'none',
-        borderRadius: 0,
-        padding: 0,
-        boxShadow: 'none',
-      },
+  },
+
+  {
+    id: 'aluASrc',
+    type: 'aluASrc',
+    data: { label: 'ALU A SRC' },
+    position: { x: 153, y: 555 },
+    parentId: 'IE',
+    extent: 'parent',
+    style: {
+      width: 90,
+      height: 30,
+      backgroundColor: 'transparent',
+      border: 'none',
+      borderRadius: 0,
+      padding: 0,
+      boxShadow: 'none',
     },
-    
-    {
-      id: 'muxB',
-      type: 'muxB',
-      data: { label: 'MUX B' },
-      position: { x: 165, y: 820 },
-      parentId: 'IE',
-      extent: 'parent',
-      style: {
-        width: 65,
-        height: 150,
-        backgroundColor: 'transparent',
-        border: 'none',
-        borderRadius: 0,
-        padding: 0,
-        boxShadow: 'none',
-      },
+  },
+
+  {
+    id: 'muxB',
+    type: 'muxB',
+    data: { label: 'MUX B' },
+    position: { x: 165, y: 820 },
+    parentId: 'IE',
+    extent: 'parent',
+    style: {
+      width: 65,
+      height: 150,
+      backgroundColor: 'transparent',
+      border: 'none',
+      borderRadius: 0,
+      padding: 0,
+      boxShadow: 'none',
     },
-    
-    {
-      id: 'aluBSrc',
-      type: 'aluBSrc',
-      data: { label: 'ALU B SRC' },
-      position: { x: 153, y: 1002 },
-      parentId: 'IE',
-      extent: 'parent',
-      style: {
-        width: 90,
-        height: 30,
-        backgroundColor: 'transparent',
-        border: 'none',
-        borderRadius: 0,
-        padding: 0,
-        boxShadow: 'none',
-      },
+  },
+
+  {
+    id: 'aluBSrc',
+    type: 'aluBSrc',
+    data: { label: 'ALU B SRC' },
+    position: { x: 153, y: 1002 },
+    parentId: 'IE',
+    extent: 'parent',
+    style: {
+      width: 90,
+      height: 30,
+      backgroundColor: 'transparent',
+      border: 'none',
+      borderRadius: 0,
+      padding: 0,
+      boxShadow: 'none',
     },
-    
-    {
-      id: 'alu',
-      type: 'alu',
-      data: { label: 'ALU' },
-      position: { x: 300, y: 655 },
-      parentId: 'IE',
-      extent: 'parent',
-      style: {
-        width: 325,
-        height: 320,
-        backgroundColor: 'transparent',
-        border: 'none',
-        borderRadius: 0,
-        padding: 0,
-        boxShadow: 'none',
-      },
+  },
+
+  {
+    id: 'alu',
+    type: 'alu',
+    data: { label: 'ALU' },
+    position: { x: 300, y: 655 },
+    parentId: 'IE',
+    extent: 'parent',
+    style: {
+      width: 325,
+      height: 320,
+      backgroundColor: 'transparent',
+      border: 'none',
+      borderRadius: 0,
+      padding: 0,
+      boxShadow: 'none',
     },
-    
-    {
-      id: 'aluOp',
-      type: 'aluOp',
-      data: { label: 'ALU OPERATION' },
-      position: { x: 410, y: 1020 },
-      parentId: 'IE',
-      extent: 'parent',
-      style: {
-        width: 105,
-        height: 30,
-        backgroundColor: 'transparent',
-        border: 'none',
-        borderRadius: 0,
-        padding: 0,
-        boxShadow: 'none',
-      },
+  },
+
+  {
+    id: 'aluOp',
+    type: 'aluOp',
+    data: { label: 'ALU OPERATION' },
+    position: { x: 410, y: 1020 },
+    parentId: 'IE',
+    extent: 'parent',
+    style: {
+      width: 105,
+      height: 30,
+      backgroundColor: 'transparent',
+      border: 'none',
+      borderRadius: 0,
+      padding: 0,
+      boxShadow: 'none',
     },
-    
-    {
-      id: 'branchUnit',
-      type: 'branchUnit',
-      data: { label: 'Branch Unit' },
-      position: { x: 300, y: 315 },
-      parentId: 'IE',
-      extent: 'parent',
-      style: {
-        width: 325,
-        height: 160,
-        backgroundColor: 'transparent',
-        border: 'none',
-        borderRadius: 0,
-        padding: 0,
-        boxShadow: 'none',
-      },
+  },
+
+  {
+    id: 'branchUnit',
+    type: 'branchUnit',
+    data: { label: 'Branch Unit' },
+    position: { x: 300, y: 315 },
+    parentId: 'IE',
+    extent: 'parent',
+    style: {
+      width: 325,
+      height: 160,
+      backgroundColor: 'transparent',
+      border: 'none',
+      borderRadius: 0,
+      padding: 0,
+      boxShadow: 'none',
     },
-    
-    {
-      id: 'brOp',
-      type: 'brOp',
-      data: { label: 'BrOp' },
-      position: { x: 418, y: 530 },
-      parentId: 'IE',
-      extent: 'parent',
-      style: {
-        width: 90,
-        height: 30,
-        backgroundColor: 'transparent',
-        border: 'none',
-        borderRadius: 0,
-        padding: 0,
-        boxShadow: 'none',
-      },
+  },
+
+  {
+    id: 'brOp',
+    type: 'brOp',
+    data: { label: 'BrOp' },
+    position: { x: 418, y: 530 },
+    parentId: 'IE',
+    extent: 'parent',
+    style: {
+      width: 90,
+      height: 30,
+      backgroundColor: 'transparent',
+      border: 'none',
+      borderRadius: 0,
+      padding: 0,
+      boxShadow: 'none',
     },
-    
-    //PIVOTS S3
-    
-    {
-      id: 'pivot4',
-      type: 'pivot4',
-      data: { label: '' },
-      position: { x: 45, y: 731 },
-      parentId: 'IE',
-      extent: 'parent',
-      style: {
-        width: 5,
-        height: 5,
-        backgroundColor: 'black',
-        border: 'none',
-        borderRadius: 0,
-        padding: 0,
-        boxShadow: 'none',
-      },
+  },
+
+  //PIVOTS S3
+
+  {
+    id: 'pivot4',
+    type: 'pivot4',
+    data: { label: '' },
+    position: { x: 45, y: 731 },
+    parentId: 'IE',
+    extent: 'parent',
+    style: {
+      width: 5,
+      height: 5,
+      backgroundColor: 'black',
+      border: 'none',
+      borderRadius: 0,
+      padding: 0,
+      boxShadow: 'none',
     },
-    
-    {
-      id: 'pivot10',
-      type: 'pivot10',
-      data: { label: '' },
-      position: { x: 44, y: 1272 },
-      parentId: 'IE',
-      extent: 'parent',
-      style: {
-        width: 5,
-        height: 5,
-        backgroundColor: 'black',
-        border: 'none',
-        borderRadius: 0,
-        padding: 0,
-        boxShadow: 'none',
-      },
+  },
+
+  {
+    id: 'pivot10',
+    type: 'pivot10',
+    data: { label: '' },
+    position: { x: 44, y: 1272 },
+    parentId: 'IE',
+    extent: 'parent',
+    style: {
+      width: 5,
+      height: 5,
+      backgroundColor: 'black',
+      border: 'none',
+      borderRadius: 0,
+      padding: 0,
+      boxShadow: 'none',
     },
-    
-    //jump
-    
-    {
-      id: 'pivotJump3',
-      type: 'pivotJump3',
-      data: { label: 'pivotJump3' },
-      position: { x: 29, y: 583 },
-      parentId: 'IE',
-      extent: 'parent',
-      style: {
-        width: 47,
-        height: 47,
-        backgroundColor: 'transparent',
-        border: 'none',
-        borderRadius: 0,
-        padding: 0,
-        boxShadow: 'none',
-      },
+  },
+
+  //jump
+
+  {
+    id: 'pivotJump3',
+    type: 'pivotJump3',
+    data: { label: 'pivotJump3' },
+    position: { x: 29, y: 583 },
+    parentId: 'IE',
+    extent: 'parent',
+    style: {
+      width: 47,
+      height: 47,
+      backgroundColor: 'transparent',
+      border: 'none',
+      borderRadius: 0,
+      padding: 0,
+      boxShadow: 'none',
     },
-    
-    {
-      id: 'pivotJump5',
-      type: 'pivotJump5',
-      data: { label: 'pivotJump5' },
-      position: { x: 27, y: 1073 },
-      parentId: 'IE',
-      extent: 'parent',
-      style: {
-        width: 47,
-        height: 47,
-        backgroundColor: 'transparent',
-        border: 'none',
-        borderRadius: 0,
-        padding: 0,
-        boxShadow: 'none',
-      },
+  },
+
+  {
+    id: 'pivotJump5',
+    type: 'pivotJump5',
+    data: { label: 'pivotJump5' },
+    position: { x: 27, y: 1073 },
+    parentId: 'IE',
+    extent: 'parent',
+    style: {
+      width: 47,
+      height: 47,
+      backgroundColor: 'transparent',
+      border: 'none',
+      borderRadius: 0,
+      padding: 0,
+      boxShadow: 'none',
     },
-    
-    {
-      id: 'pivotJump7',
-      type: 'pivotJump7',
-      data: { label: 'pivotJump7' },
-      position: { x: 27, y: 1128 },
-      parentId: 'IE',
-      extent: 'parent',
-      style: {
-        width: 47,
-        height: 47,
-        backgroundColor: 'transparent',
-        border: 'none',
-        borderRadius: 0,
-        padding: 0,
-        boxShadow: 'none',
-      },
+  },
+
+  {
+    id: 'pivotJump7',
+    type: 'pivotJump7',
+    data: { label: 'pivotJump7' },
+    position: { x: 27, y: 1128 },
+    parentId: 'IE',
+    extent: 'parent',
+    style: {
+      width: 47,
+      height: 47,
+      backgroundColor: 'transparent',
+      border: 'none',
+      borderRadius: 0,
+      padding: 0,
+      boxShadow: 'none',
     },
-    
+  },
 
 
-    
-   
-  ];
+
+
+
+];
