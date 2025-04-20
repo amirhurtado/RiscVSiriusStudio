58802d09d32075e789a2830adc7063e64f977a61
Amir Hurtado <a.hurtado@utp.edu.co>
2025-04-20 14:45:24 -0500

    BU: Implement component logic that displays the representation of values ​​using a hover

--- client/panelView/src/components/graphic/elements/IE/BranchUnit/LabelValueContainer.tsx
+++ client/panelView/src/components/graphic/elements/IE/BranchUnit/LabelValueContainer.tsx
@@ -1,45 +1,99 @@
 import { useEffect, useState } from 'react';
-import LabelValue from '../../../LabelValue';
 import { useCurrentInst } from '@/context/graphic/CurrentInstContext';
-import { binaryToHex } from '@/utils/handlerConversions';
+import LabelValueWithHover from '@/components/graphic/elements/LabelValueWithHover';
+import { binaryToHex, binaryToInt } from '@/utils/handlerConversions';
 
-const LavelValueContainer = () => {
+const LabelValueContainer = () => {
   const { currentType, currentResult } = useCurrentInst();
 
   const [hexA, setHexA] = useState('');
   const [hexB, setHexB] = useState('');
+  const [decA, setDecA] = useState('');
+  const [decB, setDecB] = useState('');
+  const [binA, setBinA] = useState('');
+  const [binB, setBinB] = useState('');
+
+  const [opHex, setOpHex] = useState('');
+  const [opBin, setOpBin] = useState('');
+  const [opDec, setOpDec] = useState('');
+
+  const [resHex, setResHex] = useState('');
+  const [resBin, setResBin] = useState('');
+  const [resDec, setResDec] = useState('');
 
   useEffect(() => {
-    if (currentResult && currentResult.bu) {
-      setHexA(binaryToHex(currentResult.bu.a).toUpperCase());
-      setHexB(binaryToHex(currentResult.bu.b).toUpperCase());
+    if (currentResult?.bu) {
+      const a = currentResult.bu.a;
+      const b = currentResult.bu.b;
+      const op = currentResult.bu.operation;
+      const res = currentResult.bu.result;
+
+      setHexA(binaryToHex(a).toUpperCase());
+      setHexB(binaryToHex(b).toUpperCase());
+      setDecA(binaryToInt(a));
+      setDecB(binaryToInt(b));
+      setBinA(a);
+      setBinB(b);
+
+      setOpHex(binaryToHex(op).toUpperCase());
+      setOpBin(op);
+      setOpDec(binaryToInt(op));
+
+      setResHex(binaryToHex(res).toUpperCase());
+      setResBin(res);
+      setResDec(binaryToInt(res));
     }
   }, [currentResult]);
 
   return (
-  
-      <>
-        { currentType === 'B' && (
-          <>
-              <div className="absolute top-[1.2rem] left-[.8rem]">
-                <LabelValue label="" value={`h'${hexB}`} />
-              </div>
+    <>
+      {currentType === 'B' && (
+        <>
+          {/* B */}
+          <LabelValueWithHover
+            label=""
+            value={`h'${hexB}`}
+            decimal={decB}
+            binary={binB}
+            hex={hexB}
+            positionClassName="absolute top-[1.2rem] left-[.8rem]"
+          />
 
-              <div className="absolute top-[5.5rem] left-[.8rem]">
-                <LabelValue label="" value={`h'${hexA}`} />
-              </div>
-         </>
-        )}
+          {/* A */}
+          <LabelValueWithHover
+            label=""
+            value={`h'${hexA}`}
+            decimal={decA}
+            binary={binA}
+            hex={hexA}
+            positionClassName="absolute top-[5.5rem] left-[.8rem]"
+          />
+        </>
+      )}
 
-        <div className="absolute bottom-[-6.43rem] right-[-.8rem]">
-          <LabelValue label="" input={false} value={`b'${currentResult.bu.operation}`} />
-        </div>
+      {/* Operation */}
+      <LabelValueWithHover
+        label=""
+        value={`b'${opBin}`}
+        decimal={opDec}
+        binary={opBin}
+        hex={opHex}
+        positionClassName="absolute bottom-[-6.43rem] right-[-.8rem]"
+        input={false}
+      />
 
-        <div className="absolute top-[3.7rem] right-[.8rem]">
-          <LabelValue label="" input={false} value={`b'${currentResult.bu.result}`} />
-        </div>
-      </>
-    )
+      {/* Result */}
+      <LabelValueWithHover
+        label=""
+        value={`b'${resBin}`}
+        decimal={resDec}
+        binary={resBin}
+        hex={resHex}
+        positionClassName="absolute top-[3.7rem] right-[.8rem]"
+        input={false}
+      />
+    </>
+  );
 };
 
-export default LavelValueContainer;
+export default LabelValueContainer;
