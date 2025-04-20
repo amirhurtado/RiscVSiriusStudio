9b0890b3213f1559075c30e9ab9c38dfbf3a1bc4
Amir Hurtado <a.hurtado@utp.edu.co>
2025-04-20 13:41:47 -0500

    MUXB: Implement component logic that displays the representation of values ​​using a hover

--- client/panelView/src/components/graphic/elements/IE/MuxB.tsx
+++ client/panelView/src/components/graphic/elements/IE/MuxB.tsx
@@ -1,12 +1,14 @@
 import MuxContainer from "../MUXContainer";
 import { Handle, Position } from "@xyflow/react";
 import { useCurrentInst } from "@/context/graphic/CurrentInstContext";
-import LabelValue from "@/components/graphic/LabelValue";
 import { useOperation } from "@/context/panel/OperationContext";
+import LabelValueWithHover from "@/components/graphic/elements/LabelValueWithHover";
 
 function MuxB() {
   const { currentResult } = useCurrentInst();
   const { operation } = useOperation();
+
+  const signal = currentResult.alub.signal;
 
   return (
     <div className="relative w-full h-full">
@@ -14,10 +16,19 @@ function MuxB() {
         <MuxContainer />
         {operation !== "uploadMemory" && (
           <div className="absolute bottom-[-2.3rem] left-[3.5rem]">
-            <LabelValue label="" value={`b'${currentResult.alub.signal}`} input={false} />
+            <LabelValueWithHover
+              label=""
+              value={`b'${signal}`}
+              decimal={signal}
+              binary={signal}
+              hex={signal}
+              input={false}
+              positionClassName=""
+            />
           </div>
         )}
       </div>
+
       <Handle
         type="target"
         id="registersUnitB"
