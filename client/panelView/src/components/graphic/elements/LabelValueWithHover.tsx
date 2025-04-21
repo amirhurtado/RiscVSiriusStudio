c937c65359c2a2ee40bb5bd6f4b1b6bc33e56469
Amir Hurtado <a.hurtado@utp.edu.co>
2025-04-21 11:19:47 -0500

    Design change in base tool type

--- client/panelView/src/components/graphic/elements/LabelValueWithHover.tsx
+++ client/panelView/src/components/graphic/elements/LabelValueWithHover.tsx
@@ -18,33 +18,36 @@ const LabelValueWithHover = ({
   hex: string;
   positionClassName: string;
   input?: boolean;
-}) => (
-  <div className={`absolute ${positionClassName}`}>
-    <HoverCard>
-      <HoverCardTrigger asChild>
-        <div>
-          <LabelValue label={label} value={value} input={input} />
-        </div>
-      </HoverCardTrigger>
-      <HoverCardContent className="!text-[.65rem] font-mono flex flex-col gap-2">
-        <div>
-          <p className="text-xs font-semibold text-slate-300">decimal:</p>
-          <hr className="my-[2px]" />
-          <p>d'{decimal}</p>
-        </div>
-        <div>
-          <p className="text-xs font-semibold text-slate-300">binary:</p>
-          <hr className="my-[2px]" />
-          <p className="break-all">b'{binary}</p>
-        </div>
-        <div>
-          <p className="text-xs font-semibold text-slate-300">hex:</p>
-          <hr className="my-[2px]" />
-          <p>h'{hex}</p>
-        </div>
-      </HoverCardContent>
-    </HoverCard>
-  </div>
-);
+}) => {
+
+  return (
+    <div className={`absolute ${positionClassName}`}>
+      <HoverCard>
+        <HoverCardTrigger asChild>
+          <div>
+            <LabelValue label={label} value={value} input={input} />
+          </div>
+        </HoverCardTrigger>
+        <HoverCardContent className="!text-[.65rem] flex flex-col gap-2 bg-[#1B1B1B] ">
+          <div>
+            <p className="text-xs font-semibold text-gray-200">decimal:</p>
+            <p>d'{decimal}</p>
+          </div>
+          <div>
+            <p className="text-xs font-semibold text-gray-200">binary:</p>
+            <p className="">b'{binary}</p>
+          </div>
+          <div>
+            <p className="text-xs font-semibold text-gray-200">hexadecimal:</p>
+            <p>h'{hex}</p>
+          </div>
+        </HoverCardContent>
+      </HoverCard>
+    </div>
+  )
+
+}
+  
+
 
 export default LabelValueWithHover;
