3eeb89e28e8f0c7a7fbc57d8051a8375e1ca8507
Amir Hurtado <a.hurtado@utp.edu.co>
2025-04-28 09:25:36 -0500

    Refactor ExportMemory component for cleaner code structure

--- client/panelView/src/components/panel/Settings/Step/ExportMemory.tsx
+++ client/panelView/src/components/panel/Settings/Step/ExportMemory.tsx
@@ -1,24 +1,12 @@
-
 import { Button } from "@/components/panel/ui/button";
 import { Save } from "lucide-react";
 
 const ExportMemory = () => {
-  
-
   return (
     <div className="flex items-center gap-2 ml-4">
-      <input
-        type="file"
-        id="fileInputExportMemory"
-        accept=".txt"
-        className="hidden"
-      />
+      <input type="file" id="fileInputExportMemory" accept=".txt" className="hidden" />
       <div className="flex items-center gap-2">
-        <Button
-          variant="outline"
-          size="icon"
-          id="exportMemoryBtn"
-        >
+        <Button variant="outline" size="icon" id="exportMemoryBtn">
           <Save strokeWidth={1} />
         </Button>
         <p className="text-gray">Memory</p>
--- client/panelView/src/components/panel/Settings/Step/ExportRegisters.tsx
+++ client/panelView/src/components/panel/Settings/Step/ExportRegisters.tsx
@@ -1,24 +1,12 @@
-
 import { Button } from "@/components/panel/ui/button";
 import { Save } from "lucide-react";
 
 const ExportRegisters = () => {
-  
-
   return (
     <div className="flex items-center gap-2">
-      <input
-        type="file"
-        id="fileInputExportRegisters"
-        accept=".txt"
-        className="hidden"
-      />
+      <input type="file" id="fileInputExportRegisters" accept=".txt" className="hidden" />
       <div className="flex items-center gap-2">
-        <Button
-          variant="outline"
-          size="icon"
-          id="exportRegistersBtn"
-        >
+        <Button variant="outline" size="icon" id="exportRegistersBtn">
           <Save strokeWidth={1} />
         </Button>
         <p className="text-gray">Registers</p>
