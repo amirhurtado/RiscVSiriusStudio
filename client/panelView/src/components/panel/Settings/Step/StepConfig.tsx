5980f1d93d9eabc220cc18c0580455d607b38792
Amir Hurtado <a.hurtado@utp.edu.co>
2025-04-28 11:00:43 -0500

    Code is implemented to import memory (part of the program) in hexadecimal V2

--- client/panelView/src/components/panel/Settings/Step/StepConfig.tsx
+++ client/panelView/src/components/panel/Settings/Step/StepConfig.tsx
@@ -1,25 +1,14 @@
 import MemorySizeInput from "../MemorySizeInput";
-import ExportMemory from "./ExportMemory";
-import ExportRegisters from "./ExportRegisters";
 
 const StepConfig = () => {
   return (
     <div className="flex flex-col gap-7">
-        <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-3">
         <p className="font-semibold">Memory size (In bytes)</p>
-            <MemorySizeInput disabled={true}/>
-        </div>
-        <div className="flex flex-col gap-2 mt-1">
-        <p>Export data</p>
-        <div className="flex items-center gap-3 mt-2">
-
-          <ExportRegisters />
-          <ExportMemory />
-        </div>
+        <MemorySizeInput disabled={true} />
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default StepConfig
+export default StepConfig;
