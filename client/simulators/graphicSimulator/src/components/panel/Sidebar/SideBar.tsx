e778bc15a3b93efad32f320218dca8dd94f6c259
Amir Hurtado <a.hurtado@utp.edu.co>
2025-04-29 16:10:34 -0500

    Refactor Sidebar component for improved readability and structure

--- client/simulators/graphicSimulator/src/components/panel/Sidebar/SideBar.tsx
+++ client/simulators/graphicSimulator/src/components/panel/Sidebar/SideBar.tsx
@@ -25,75 +25,66 @@ export function SideBar() {
   const { section, setSection } = useSection();
   const { typeSimulator } = useSimulator();
   return (
-    <Sidebar >
+    <Sidebar>
       <SidebarContent className="relative w-full h-full pr-10 overflow-x-hidden overflow-y-auto hide-scrollbar">
         <SidebarGroup className="flex flex-col flex-1">
           <SidebarGroupLabel>Options</SidebarGroupLabel>
           <SidebarGroupContent className="flex flex-col">
             <SidebarMenu className="flex flex-col justify-between h-auto pl-3 mt-1">
-
-            <div className="flex flex-col items-start gap-3">
-            {((operation === "uploadMemory" || operation === "step") && typeSimulator === "graphic") && (
-                <SidebarMenuItem className="flex items-center gap-1">
-                 <a onClick={() =>  setSection('program')} className="curser-pointer">
-                  
-                       <Button variant="outline" size="icon">
-                         <Text />
-                       </Button>
-                 </a>
-                 {section === "program" && <CircleActive />}
-               </SidebarMenuItem>
-               )}
-
-              { !(operation === "") && !(operation === "uploadMemory") && (
-                <SidebarMenuItem className="flex items-center gap-1">
-                  <a onClick={() => setSection('search')} className="curser-pointer">
+              <div className="flex flex-col items-start gap-3">
+                {(operation === "uploadMemory" || operation === "step") &&
+                  typeSimulator === "graphic" && (
+                    <SidebarMenuItem className="flex items-center gap-1">
+                      <a onClick={() => setSection("program")} className="curser-pointer">
                         <Button variant="outline" size="icon">
-                          <Search />
+                          <Text />
                         </Button>
-                  
-                  </a>
-                  {section === "search" && <CircleActive />}
-                </SidebarMenuItem>
-              )}
+                      </a>
+                      {section === "program" && <CircleActive />}
+                    </SidebarMenuItem>
+                  )}
 
-              
-              {!(operation === "uploadMemory") && (
-                <SidebarMenuItem className="flex items-center gap-1">
-                  <a onClick={() => setSection('convert')} className="curser-pointer">
-                        <Button variant="outline" size="icon">
-                          <Calculator />
-                        </Button>
-                  
-                  </a>
-                  {section === "convert" && <CircleActive />}
-                </SidebarMenuItem>
-               
-              )}
-           
+                {!(operation === "") && !(operation === "uploadMemory") && (
+                  <SidebarMenuItem className="flex items-center gap-1">
+                    <a onClick={() => setSection("search")} className="curser-pointer">
+                      <Button variant="outline" size="icon">
+                        <Search />
+                      </Button>
+                    </a>
+                    {section === "search" && <CircleActive />}
+                  </SidebarMenuItem>
+                )}
 
-              {(operation === "uploadMemory" || operation === "step") && (
-               <SidebarMenuItem className="flex items-center gap-1">
-                <a onClick={() =>  setSection('settings')} className="curser-pointer">
-                 
+                {!(operation === "uploadMemory") && (
+                  <SidebarMenuItem className="flex items-center gap-1">
+                    <a onClick={() => setSection("convert")} className="curser-pointer">
+                      <Button variant="outline" size="icon">
+                        <Calculator />
+                      </Button>
+                    </a>
+                    {section === "convert" && <CircleActive />}
+                  </SidebarMenuItem>
+                )}
+
+                {(operation === "uploadMemory" || operation === "step") && (
+                  <SidebarMenuItem className="flex items-center gap-1">
+                    <a onClick={() => setSection("settings")} className="curser-pointer">
                       <Button variant="outline" size="icon">
                         <Settings />
                       </Button>
-                </a>
-                {section === "settings" && <CircleActive />}
-              </SidebarMenuItem>
-              )}
-             </div>
+                    </a>
+                    {section === "settings" && <CircleActive />}
+                  </SidebarMenuItem>
+                )}
+              </div>
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
 
         <SidebarGroup className="mb-[.5rem]">
-
           <div className="flex flex-col gap-5 pl-3 mt-2">
-
             <SidebarMenuItem className="flex items-center gap-1">
-              <a onClick={() =>  setSection('help')} className="curser-pointer">
+              <a onClick={() => setSection("help")} className="curser-pointer">
                 <Button variant="outline" size="icon">
                   <Info />
                 </Button>
