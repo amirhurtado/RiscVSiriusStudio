8c731801134b9d1ce8b6c54db1e765b8de6940f4
Amir Hurtado <a.hurtado@utp.edu.co>
2025-05-01 19:01:42 -0500

    Fix: Update IMMSrc title color based on current instruction type

--- client/simulator/src/components/graphic/elements/ID/ImmSRC.tsx
+++ client/simulator/src/components/graphic/elements/ID/ImmSRC.tsx
@@ -1,14 +1,16 @@
 import { Handle, Position } from '@xyflow/react';
 import { useOverlay } from '@/context/graphic/OverlayContext';
+import { useCurrentInst } from '@/context/graphic/CurrentInstContext';
 
 export default function ImmSrc() {
   const { overlayDecodeActive} = useOverlay();
+  const { currentType } = useCurrentInst();
 
   return (
     <div className='w-full'>
 
        <div className='relative w-full h-full'>
-       <h2 className={`titleInElement right-[.7rem] top-[40%] -translate-y-[40%]  ${overlayDecodeActive && 'overlay-scale'}`}>IMMSrc</h2>
+       <h2 className={`titleInElement right-[.7rem] top-[40%] -translate-y-[40%]  ${overlayDecodeActive && 'overlay-scale'} ${(currentType === "R") && "!text-[#D3D3D3]"}`}>IMMSrc</h2>
        </div>
         
        <div  className={`${overlayDecodeActive && 'overlay-moveY'}`} >
