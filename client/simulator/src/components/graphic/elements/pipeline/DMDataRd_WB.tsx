fc4056b4709a1cdea651c9f0ea820e79618692eb
Amir Hurtado <a.hurtado@utp.edu.co>
2025-05-27 21:53:19 -0500

    feat: Add Handle components to DMDataRd_WB for input and output connections

--- client/simulator/src/components/graphic/elements/pipeline/DMDataRd_WB.tsx
+++ client/simulator/src/components/graphic/elements/pipeline/DMDataRd_WB.tsx
@@ -1,9 +1,19 @@
 import ContainerPipelineRegister from '../ContainerPipelineRegister'
+import { Handle, Position } from "@xyflow/react";
+
 
 const DMDataRd_WB = () => {
   return (
     <div className='relative w-full overflow-visible'>
       <ContainerPipelineRegister text='DMDataRd_wb' top={true}/>
+      <Handle type="target" position={Position.Left} className="input" style={{ top: "2.2rem" }} />
+      
+            <Handle
+              type="source"
+              position={Position.Right}
+              className="output"
+              style={{ right: "-2.4rem", top: "2.2rem" }}
+            />
   
     </div>
   )
