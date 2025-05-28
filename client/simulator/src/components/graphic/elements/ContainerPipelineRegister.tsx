788262f277d77bbf1c7a582635cb94e4bab5061d
Amir Hurtado <a.hurtado@utp.edu.co>
2025-05-28 11:34:44 -0500

    feat: Enhance ContainerPipelineRegister component structure; add height prop for dynamic sizing

--- client/simulator/src/components/graphic/elements/ContainerPipelineRegister.tsx
+++ client/simulator/src/components/graphic/elements/ContainerPipelineRegister.tsx
@@ -1,13 +1,28 @@
-import ClockTriangle from "../ClockTriangle"
+import ClockTriangle from "../ClockTriangle";
 
-const ContainerPipelineRegister = ({text, top} : {text : string, top? :  boolean}) => {
+const ContainerPipelineRegister = ({
+  text,
+  top,
+  height,
+}: {
+  text: string;
+  top?: boolean;
+  height?: number;
+}) => {
   return (
-    <div className="relative w-[4.5rem] h-[4.5rem] bg-amber-200 border-4 border-black">
-            <p className={`absolute ${top ? 'top-[-2.9rem]' : 'bottom-[-2.7rem]'} text-[1.4rem] text-orange-400 font-semibold left-[50%] transform -translate-x-[50%]`}>{text}</p>
-            <ClockTriangle pipeline={true}/>
-      
+    <div
+      className={`relative w-[4.5rem] ${
+        height ? `h-[${height}]` : "h-[4.5rem]"
+      } bg-amber-200 border-4 border-black`}>
+      <p
+        className={`absolute ${
+          top ? "top-[-2.9rem]" : "bottom-[-2.7rem]"
+        } text-[1.4rem] text-orange-400 font-semibold left-[50%] transform -translate-x-[50%]`}>
+        {text}
+      </p>
+      <ClockTriangle pipeline={true} />
     </div>
-  )
-}
+  );
+};
 
-export default ContainerPipelineRegister
+export default ContainerPipelineRegister;
