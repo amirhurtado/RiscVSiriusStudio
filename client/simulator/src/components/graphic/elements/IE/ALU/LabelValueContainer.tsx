1706354314728453ecf84c46f9ef89167b684683
Amir Hurtado <a.hurtado@utp.edu.co>
2025-06-16 16:16:12 -0500

    It is placed on the aluminum component that is being operated on.

--- client/simulator/src/components/graphic/elements/IE/ALU/LabelValueContainer.tsx
+++ client/simulator/src/components/graphic/elements/IE/ALU/LabelValueContainer.tsx
@@ -3,21 +3,53 @@ import { useCurrentInst } from "@/context/graphic/CurrentInstContext";
 import LabelValueWithHover from "@/components/graphic/elements/LabelValueWithHover";
 import { binaryToHex, binaryToInt } from "@/utils/handlerConversions";
 
+
 const aluOperations: Record<string, string> = {
+  // Operaciones clásicas (4 o 5 bits según el caso)
   "0000": "A + B",
+  "00000": "A + B",
+
   "1000": "A - B",
+  "01000": "A - B",
+
   "0100": "A ⊕ B",
+  "00100": "A ⊕ B",
+
   "0110": "A | B",
+  "00110": "A | B",
+
   "0111": "A & B",
+  "00111": "A & B",
+
   "0001": "A << B",
+  "00001": "A << B",
+
   "0101": "A >> B",
-  "1101": "A >> B",
-  "0010": "A < B",
-  "0011": "A < B",
+  "00101": "A >> B",
+
+  "1101": "A >> B (Arithmetic)",
+  "01101": "A >> B (Arithmetic)",
+
+  "0010": "A < B (Unsigned)",
+  "00010": "A < B (Unsigned)",
+
+  "0011": "A < B (Signed)",
+  "00011": "A < B (Signed)",
+
+  "10000": "A * B",
+  "10001": "High bits of A * B (Signed)",
+  "10010": "High bits of A * B (Signed × Unsigned)",
+  "10011": "High bits of A * B (Unsigned)",
+  "10100": "A ÷ B (Signed)",
+  "10101": "A ÷ B (Unsigned)",
+  "10110": "A % B (Signed)",
+  "10111": "A % B (Unsigned)",
 };
 
 const LabelValueContainer = () => {
   const { currentType, currentResult } = useCurrentInst();
+
+  
 
   const [aHex, setAHex] = useState("");
   const [aBin, setABin] = useState("");
@@ -53,8 +85,9 @@ const LabelValueContainer = () => {
 
   const aluOp = currentResult?.alu?.operation ?? "";
   const operationDescription = aluOperations[aluOp];
-  const showMsbNote = aluOp === "1101";
-  const showZeroExtend = aluOp === '0011'
+
+
+
 
   return (
     <>
@@ -100,10 +133,10 @@ const LabelValueContainer = () => {
         hex={parseInt(aluOp, 2).toString(16).toUpperCase()}
         input={false}
         positionClassName="bottom-[-6rem] right-[0]"
-        operation={operationDescription}
-        showMsbNote={showMsbNote}
-        showZeroExtend={showZeroExtend}
+
       />
+
+      <p className="absolute bottom-[1rem] left-[50%] transform -translate-x-[50%] z-1000 text-xl text-black">{operationDescription}</p>
     </>
   );
 };
