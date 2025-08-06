f90d3946bb66191f9fce5f9f6afa7961d051ada7
Amir Hurtado <a.hurtado@utp.edu.co>
2025-08-06 00:56:00 -0500

    Fix conditional rendering of StagesPipeline based on typeSimulator

--- client/simulator/src/components/panel/Sections/Tables/Tables.tsx
+++ client/simulator/src/components/panel/Sections/Tables/Tables.tsx
@@ -1,16 +1,17 @@
-import RegisterTable from "./RegisterTable"
-import MemoryTable from "./MemoryTable"
-import StagesPipeline from "./StagesPipeline"
+import RegisterTable from "./RegisterTable";
+import MemoryTable from "./MemoryTable";
+import StagesPipeline from "./StagesPipeline";
+import { useSimulator } from "@/context/shared/SimulatorContext";
 
 const Tables = () => {
+  const { typeSimulator } = useSimulator();
   return (
     <div className="flex gap-5 overflow-hidden min-w-min">
-        <RegisterTable />
-        <MemoryTable />
-        <StagesPipeline />
-    
+      <RegisterTable />
+      <MemoryTable />
+      {typeSimulator === "pipeline" && (<StagesPipeline />)}
     </div>
-  )
-}
+  );
+};
 
-export default Tables
+export default Tables;
