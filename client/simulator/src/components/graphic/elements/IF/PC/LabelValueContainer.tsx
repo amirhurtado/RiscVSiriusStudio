981454824301b0092486bac525dcaebaa1bf4495
Amir Hurtado <a.hurtado@utp.edu.co>
2025-08-24 17:14:06 -0500

    Implementation of values ​​in each unit, (PC)

--- client/simulator/src/components/graphic/elements/IF/PC/LabelValueContainer.tsx
+++ client/simulator/src/components/graphic/elements/IF/PC/LabelValueContainer.tsx
@@ -1,48 +1,63 @@
-import { useEffect, useState } from "react";
 import { useCurrentInst } from "@/context/graphic/CurrentInstContext";
 import { binaryToHex, binaryToInt } from "@/utils/handlerConversions";
-import { useFormattedPC } from "@/hooks/graphic/useFormattedPC";
 import LabelValueWithHover from "../../LabelValueWithHover";
+import { useSimulator } from "@/context/shared/SimulatorContext";
 
 const LabelValueContainer = () => {
-  const { currentMonocycleResult, currentMonocycletInst } = useCurrentInst();
+  const { currentMonocycleResult, currentMonocycletInst, pipelineValuesStages } = useCurrentInst();
+  const { typeSimulator } = useSimulator();
 
-  
- const formattedPC = useFormattedPC(currentMonocycletInst?.currentPc ?? 0);
+  let pcDec = "--", pcBin = "--", pcHex = "--";
+  let nextPcDec = "--", nextPcBin = "--", nextPcHex = "--";
+  let displayPC = "--";
 
-
-  const [hexNextPC, setHexNextPC] = useState("");
-  const [decNextPC, setDecNextPC] = useState("");
-  const [binNextPC, setBinNextPC] = useState("");
-
-  useEffect(() => {
+  if (typeSimulator === "monocycle") {
+    if (currentMonocycletInst) {
+      const pcValue = currentMonocycletInst.currentPc;
+      pcBin = pcValue.toString(2).padStart(32, "0");
+      pcDec = pcValue.toString();
+      pcHex = binaryToHex(pcBin).toUpperCase();
+      displayPC = `h'${pcHex}`;
+    }
     if (currentMonocycleResult?.buMux?.result) {
       const bin = currentMonocycleResult.buMux.result;
-      setHexNextPC(binaryToHex(bin).toUpperCase());
-      setDecNextPC(binaryToInt(bin));
-      setBinNextPC(bin.padStart(32, "0"));
+      nextPcHex = binaryToHex(bin).toUpperCase();
+      nextPcDec = binaryToInt(bin).toString();
+      nextPcBin = bin.padStart(32, "0");
     }
-  }, [currentMonocycleResult]);
+  } else {
+    if (pipelineValuesStages) {
+      const pcValue = pipelineValuesStages.IF.PC;
+      if (pcValue >= 0) {
+        pcBin = pcValue.toString(2).padStart(32, "0");
+        pcDec = pcValue.toString();
+        pcHex = binaryToHex(pcBin).toUpperCase();
+        displayPC = `h'${pcHex}`;
+      }
 
-  const pcValue = parseInt(String(currentMonocycletInst?.currentPc), 2) * 4;
-  const pcBin = pcValue.toString(2).padStart(32, "0");
-  const pcDec = pcValue.toString();
-  const pcHex = binaryToHex(pcBin).toUpperCase();
+      const nextPcValue = pipelineValuesStages.IF.PCP4;
+      if (nextPcValue >= 0) {
+        nextPcBin = nextPcValue.toString(2).padStart(32, "0");
+        nextPcDec = nextPcValue.toString();
+        nextPcHex = binaryToHex(nextPcBin).toUpperCase();
+      }
+    }
+  }
 
   return (
     <>
       <LabelValueWithHover
         label="NextPc"
-        value={`h'${hexNextPC}`}
-        decimal={decNextPC}
-        binary={binNextPC}
-        hex={hexNextPC}
+        value={`h'${nextPcHex}`}
+        decimal={nextPcDec}
+        binary={nextPcBin}
+        hex={nextPcHex}
         positionClassName="top-[8rem] left-[.8rem]"
       />
 
       <LabelValueWithHover
         label="PC"
-        value={formattedPC}
+        value={displayPC}
         decimal={pcDec}
         binary={pcBin}
         hex={pcHex}
