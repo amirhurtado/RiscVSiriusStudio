63d2c46b1454386432176b59cfcaf8e427b8fa3f
Amir Hurtado <a.hurtado@utp.edu.co>
2025-08-25 15:39:29 -0500

    Implementation of refactored view modification controls

--- client/simulator/src/components/graphic/custom/CustomControls.tsx
+++ client/simulator/src/components/graphic/custom/CustomControls.tsx
@@ -1,11 +1,15 @@
 import { Controls } from '@xyflow/react';
 import { RedoDot, Ban, RotateCcw, ZoomIn, ZoomOut, Fullscreen } from 'lucide-react';
 import DownloadButton from '../DownloadButton';
-
 import { sendMessage } from '@/components/Message/sendMessage';
 import { useDialog } from '@/context/panel/DialogContext';
 import { useEffect, useState } from 'react';
 
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from '@/components/ui/hover-card';
 
 interface CustomControlsProps {
   onToggleMinimap: () => void;
@@ -19,52 +23,75 @@ export default function CustomControls({
   onFitView,
   onZoomIn,
   onZoomOut,
-  // onToggleInteractive,
 }: CustomControlsProps) {
   const { dialog } = useDialog();
   const [showControls, setShowControls] = useState(true);
 
-
   useEffect(() => {
-    if(dialog && dialog.stop){
+    if (dialog && dialog.stop) {
       setShowControls(false);
     }
-  }, [dialog])
+  }, [dialog]);
 
   return (
-    <Controls
-      showFitView={false}       
-      showZoom={false}          
-      showInteractive={false} 
-    >
-      { showControls && (
+    <Controls showFitView={false} showZoom={false} showInteractive={false}>
+      {showControls && (
         <>
-          <button className="react-flow__controls-button-custom_blue" onClick={() => sendMessage({event:"step"})}  title="Step">
-            <RedoDot size={18}  />
+          <button
+            className="react-flow__controls-button-custom_blue"
+            onClick={() => sendMessage({ event: 'step' })}
+            title="Step"
+          >
+            <RedoDot size={18} />
           </button>
-          <button className="react-flow__controls-button-custom_red " onClick={() => sendMessage({event:"stop"})}   title="Stop">
-            <Ban size={16}  />
+          <button
+            className="react-flow__controls-button-custom_red"
+            onClick={() => sendMessage({ event: 'stop' })}
+            title="Stop"
+          >
+            <Ban size={16} />
           </button>
-          <button className="react-flow__controls-button-custom_green" onClick={() => sendMessage({event:"reset"})}  title="Reset">
-            <RotateCcw size={16}  />
+          <button
+            className="react-flow__controls-button-custom_green"
+            onClick={() => sendMessage({ event: 'reset' })}
+            title="Reset"
+          >
+            <RotateCcw size={16} />
           </button>
         </>
       )}
-      <button className="react-flow__controls-button-custom" onClick={onZoomIn} title="Zoom In">
-        <ZoomIn size={16} />
-      </button>
-      <button className="react-flow__controls-button-custom" onClick={onZoomOut} title="Zoom Out">
-        <ZoomOut  size={16}  />
-      </button>
 
-      <button className="react-flow__controls-button-custom" onClick={onFitView} title="FitView">
-        <Fullscreen size={16}  />
-      </button>
-
-      {/* <button className="react-flow__controls-button-custom" onClick={onToggleInteractive} title="Toggle Interactive">
-        <Lock size={16}  />
-      </button> */}
-
+      <HoverCard openDelay={200} closeDelay={100}>
+        <HoverCardTrigger asChild>
+          <button
+            className="react-flow__controls-button-custom"
+            onClick={onFitView}
+            title="Fit View"
+          >
+            <Fullscreen size={16} />
+          </button>
+        </HoverCardTrigger>
+        
+        <HoverCardContent side="right" align="center" className="w-auto p-0 ml-2 border-none bg-transparent">
+          <div className="flex bg-white rounded-lg shadow-md react-flow__controls">
+            <button
+              className="react-flow__controls-button-custom"
+              onClick={onZoomIn}
+              title="Zoom In"
+            >
+              <ZoomIn size={16} />
+            </button>
+            <button
+              className="react-flow__controls-button-custom"
+              onClick={onZoomOut}
+              title="Zoom Out"
+            >
+              <ZoomOut size={16} />
+            </button>
+          </div>
+        </HoverCardContent>
+      </HoverCard>
+      
       <DownloadButton />
     </Controls>
   );
