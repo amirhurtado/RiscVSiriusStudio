326e3deddfd2a4a1cc57d8967316af14b90dda6d
Amir Hurtado <a.hurtado@utp.edu.co>
2025-08-24 21:20:18 -0500

    Hide the information of each stage when I am in upload memory

--- client/simulator/src/components/graphic/elements/IE/InstIEPipeline.tsx
+++ client/simulator/src/components/graphic/elements/IE/InstIEPipeline.tsx
@@ -2,10 +2,10 @@ import { useCurrentInst } from "@/context/graphic/CurrentInstContext";
 import { useSimulator } from "@/context/shared/SimulatorContext";
 
 const InstIEPipeline = () => {
-  const { typeSimulator } = useSimulator();
+  const { typeSimulator, operation} = useSimulator();
   const { pipelineValuesStages } = useCurrentInst();
 
-  if (typeSimulator === "monocycle") return;
+  if (typeSimulator === "monocycle" || operation === "uploadMemory") return;
 
   return (
     <div className=" flex items-center gap-5 ml-1 h-full bg-[#66939E] px-[1.2rem] py-[.7rem] rounded-[.6rem] text-white max-w-max">
